c26590a7f87b6c2c8576f717fa0ae63a69fd9d9b
Mark Shinwell <mshinwell@janestreet.com>
2014-08-20 16:58:26 +0100

    stuff

--- asmcomp/dwarf_low/proto_die.ml
+++ asmcomp/dwarf_low/proto_die.ml
@@ -28,7 +28,9 @@ type t = {
 }
 
 let sort_attribute_values ~attribute_values =
-  List.sort Attribute.compare (List.map Attribute_value.attribute attribute_values)
+  List.sort (fun av1 av2 ->
+    Attribute.compare (Attribute_value.attribute av1) (Attribute_value.attribute av2))
+    attribute_values
 
 let create ~parent ~tag ~attribute_values =
   begin match parent with
