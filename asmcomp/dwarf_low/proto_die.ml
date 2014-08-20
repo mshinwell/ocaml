34bc0e8ddc9ca3e2b2ed48d83a31fc6500ba6140
Mark Shinwell <mshinwell@janestreet.com>
2014-08-20 12:44:14 +0100

    add files

---
+++ asmcomp/dwarf_low/proto_die.ml
@@ -0,0 +1,76 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                               OCaml                                 *)
+(*                                                                     *)
+(*                 Mark Shinwell, Jane Street Europe                   *)
+(*                                                                     *)
+(*  Copyright 2014, Jane Street Holding                                *)
+(*                                                                     *)
+(*  Licensed under the Apache License, Version 2.0 (the "License");    *)
+(*  you may not use this file except in compliance with the License.   *)
+(*  You may obtain a copy of the License at                            *)
+(*                                                                     *)
+(*      http://www.apache.org/licenses/LICENSE-2.0                     *)
+(*                                                                     *)
+(*  Unless required by applicable law or agreed to in writing,         *)
+(*  software distributed under the License is distributed on an        *)
+(*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,       *)
+(*  either express or implied.  See the License for the specific       *)
+(*  language governing permissions and limitations under the License.  *)
+(*                                                                     *)
+(***********************************************************************)
+
+type t = {
+  mutable children : t list;
+  tag : Tag.t;
+  attribute_values : Attribute_value.t list;
+  die_label : string;  (* for references between DIEs in the emitted DWARF *)
+}
+
+let die_label_counter = ref 0
+
+let create ~parent ~tag ~attribute_values =
+  begin match parent with
+  | None ->
+    if tag <> Tag.compile_unit then
+      failwith "only compilation unit proto-DIEs may be without parents"
+  | Some parent ->
+    if Tag.child_determination parent.tag = Child_determination.no then
+      failwith "attempt to attach proto-DIE to proto-DIE that \
+                never has children"
+  end;
+  let die_label = Printf.sprintf "DIE_%d" !die_label_counter in
+  incr die_label_counter;
+  (* Insert DW_AT_sibling to point at any next sibling of [t].  (Section 2.3,
+     DWARF-4 spec).  The order of siblings probably matters; we make sure that
+     it is preserved by the use of :: below and within [depth_first_fold]. *)
+  let attribute_values =
+    match parent.children with
+    | [] -> attribute_values
+    | next_sibling_of_t ->
+      (Attribute_value.create_sibling ~die_label:next_sibling_of_t.die_label)
+        :: t.attribute_values
+  in
+  let t =
+    { children = [];
+      tag;
+      attribute_values;
+      die_label;
+    }
+  in
+  parent.children <- t :: parent.children;
+  t
+
+let rec depth_first_fold t ~init ~f =
+  let children =
+    match t.children with
+    | [] -> Child_determination.no
+    | _ -> Child_determination.yes
+  in
+  let acc = f init (`DIE (t.tag, children, t.attribute_values)) in
+  let rec traverse_children ts ~acc =
+    match ts with
+    | [] -> f acc `End_of_siblings
+    | t::ts -> traverse_children ts ~acc:(depth_first_fold t ~init:acc ~f)
+  in
+  traverse_children t.children ~acc
---
+++ asmcomp/dwarf_low/proto_die.mli
@@ -0,0 +1,59 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                               OCaml                                 *)
+(*                                                                     *)
+(*                 Mark Shinwell, Jane Street Europe                   *)
+(*                                                                     *)
+(*  Copyright 2014, Jane Street Holding                                *)
+(*                                                                     *)
+(*  Licensed under the Apache License, Version 2.0 (the "License");    *)
+(*  you may not use this file except in compliance with the License.   *)
+(*  You may obtain a copy of the License at                            *)
+(*                                                                     *)
+(*      http://www.apache.org/licenses/LICENSE-2.0                     *)
+(*                                                                     *)
+(*  Unless required by applicable law or agreed to in writing,         *)
+(*  software distributed under the License is distributed on an        *)
+(*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,       *)
+(*  either express or implied.  See the License for the specific       *)
+(*  language governing permissions and limitations under the License.  *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* A "proto-DIE" contains similar information to a DWARF debugging
+   information entry (DIE), but contains the actual attributes whose values
+   are being specified, rather than linking to them via an abbrevation
+   code.  The abbreviation codes are assigned later, when the proto-DIEs
+   are turned into DIEs.
+   Proto-DIEs may be formed into the required tree structures using a
+   simple parenting relationship.  The complexities of flattening the
+   structure to the DWARF representation (Section 2.3, DWARF-4 spec) are
+   hidden. *)
+
+type t
+
+(* It is an error for [parent] to be [None] unless the [tag] is that for
+   a compilation unit (which is a top-level entity). *)
+val create
+   : parent:t option
+  -> tag:Tag.t
+  -> attribute_values:Attribute_value.t list
+  -> t
+
+(* [depth_first_fold] traverses a proto-DIE tree in a depth-first order
+   convenient for DWARF information emission.  (Section 2.3, DWARF-4 spec.)
+   [`End_of_siblings] indicates that a chain of siblings---that is to say,
+   proto-DIEs with the same parent and at the same tree depth as each other---
+   has finished.  This should correspond exactly to the points at which a
+   "chain of sibling entries [must be] terminated by a null entry" specified
+   in the DWARF-4 spec.
+*)
+val depth_first_fold
+   : t
+  -> init:'a
+  -> f:('a
+    -> [ `DIE of Tag.t * Child_determination.t * (Attribute_value.t list)
+       | `End_of_siblings
+       ]
+    -> 'a)
+  -> 'a
