794a09a05ea4a58697241cb01cbc6b580d96f602
Mark Shinwell <mshinwell@janestreet.com>
2015-04-23 11:20:43 +0100

    more work

--- asmcomp/cmm.mli
+++ asmcomp/cmm.mli
@@ -52,7 +52,7 @@ type memory_chunk =
 
 type operation =
     Capply of machtype * Debuginfo.t
-  | Cextcall of string * machtype list * machtype * bool * Debuginfo.t
+  | Cextcall of string * machtype * bool * Debuginfo.t
   | Cload of memory_chunk
   | Calloc
   | Cstore of memory_chunk
