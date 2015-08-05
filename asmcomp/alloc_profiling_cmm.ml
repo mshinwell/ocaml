3a624237124cc56082a921402f6edd903f690cd1
Mark Shinwell <mshinwell@janestreet.com>
2015-08-05 12:17:14 +0100

    allocation profiling work

--- asmcomp/alloc_profiling_cmm.ml
+++ asmcomp/alloc_profiling_cmm.ml
@@ -20,35 +20,23 @@
 (*                                                                     *)
 (***********************************************************************)
 
-let size_in_words_of_trie_node ~num_allocation_points ~call_types =
-  let module C = Cmm.Call_type in
-  let direct =
-    C.Table.find_default call_types C.Direct_call ~default:0
-  in
-  let external_direct =
-    C.Table.find_default call_types C.External_direct_call ~default:0
-  in
-  let indirect =
-    C.Table.find_default call_types C.Indirect_call ~default:0
-  in
-  let non_self_direct_tail =
-    C.Table.find_default call_types C.Non_self_direct_tailcall ~default:0
-  in
-  let indirect_tail =
-    C.Table.find_default call_types C.Indirect_tailcall ~default:0
-  in
-  let direct_calls = direct + external_direct + non_self_direct_tail in
-  let indirect_calls = indirect + indirect_tail in
-  2*num_allocation_points + direct_calls + 2*indirect_calls
+let size_in_words_of_trie_node ~num_instrumented_alloc_points
+      ~num_direct_call_points =
+  2*num_instrumented_alloc_points + 2*num_direct_call_points
 
 let code_to_allocate_trie_node ~num_allocation_points ~call_types =
   let size = size_in_words_of_trie_node ~num_allocation_points ~call_types in
   let node = Ident.create "node" in
+  let header = Cmmgen.black_block_header Obj.abstract_tag size in
   let open Cmm in
-  Cop (Cextcall ("calloc", [| Int |], false, Debuginfo.none),
-    [Cconst_int size;
-     Cconst_int Sys.word_size;
-    ])
+  Clet (node,
+    Cop (Cextcall ("calloc", [| Int |], false, Debuginfo.none),
+      [Cconst_int (1 + size);  (* "1 + " for a header *)
+       Cconst_int Sys.word_size;
+      ]),
+    Csequence (
+      Cop (Cstore Word, [node, Cconst_natint header]);
+      Cop (Caddi, [node, Cconst_int Arch.size_addr])))
 
 let function_prologue ~num_allocation_points ~call_types =
   let node_hole = Ident.create "node_hole" in
@@ -112,7 +100,7 @@ let code_for_allocation_point ~value's_header ~alloc_point_number ~node =
     (* When a new profinfo value is required, we obtain the current
        program counter, and store it together with a fresh profinfo value
        into the current trie node. *)
-    Clet (pc, Cop (Cprogram_counter, []),
+    Clet (pc, Cop (Cor, [Cprogram_counter; Cconst_int 1]),
       Clet (new_profinfo,
         Clet (new_profinfo', Cop (Cload Word, [profinfo_counter]),
           Cifthenelse (
@@ -148,92 +136,20 @@ let code_for_allocation_point ~value's_header ~alloc_point_number ~node =
 let code_for_direct_call ~node ~num_instrumented_alloc_points ~callee
       ~direct_call_point_index =
   let offset_in_trie_node_in_words =
-    1 + num_instrumented_alloc_points*2 + direct_call_point_index*2
+    num_instrumented_alloc_points*2 + direct_call_point_index*2
   in
-  let within_node =
+  let place_within_node =
     Cop (Caddi, [
       node;
-      Cconst_int (offset_in_trie_node * Sys.word_size);
+      Cconst_int (offset_in_trie_node_in_words * Arch.size_addr);
     ]
   in
-  Csequence (
-    Cop (Cstore Word, [within_node; Cprogram_counter]),
-    Cop (Calloc_profiling_load_node_hole_ptr,
-      [Cop (Caddi, [within_node; Sys.word_size])]))
-
-let instrument_function_body expr ~backtrace_bucket ~fun_name =
-  let next_alloc_point_number = ref 0 in
-  (* CR mshinwell: misleading variable name *)
-  let contains_calls = ref false in
-  let rec instrument_headers expr =
-    (* Instrument the calculation of block headers so as to insert profiling
-       information. *)
-    match expr with
-    | Cblockheader value's_header ->
-      let alloc_point_number = !next_alloc_point_number in
-      incr next_alloc_point_number;
-      code_for_allocation_point ~value's_header ~alloc_point_number
-        ~backtrace_bucket
-    | (Cconst_int _ | Cconst_natint _ | Cconst_float _ | Cconst_symbol _
-      | Cconst_pointer _ | Cconst_natpointer _ | Cvar _) -> expr
-    | Clet (id, e1, e2) ->
-      Clet (id, instrument_headers e1, instrument_headers e2)
-    | Cassign (id, e) ->
-      Cassign (id, instrument_headers e)
-    | Ctuple es ->
-      Ctuple (List.map instrument_headers es)
-    | Cop (op, es) ->
-      begin match op with
-      (* CR mshinwell: what about bounds checks? *)
-      | Calloc -> contains_calls := true
-      | Capply _ ->
-        begin match es with
-        | (Cconst_symbol s)::_ when s = fun_name -> ()
-        | _ -> contains_calls := true
-        end
-      | Cextcall (_, _, true, _) -> contains_calls := true
-      | _ -> ()
-      end;
-      Cop (op, List.map instrument_headers es)
-    | Csequence (e1, e2) ->
-      Csequence (instrument_headers e1, instrument_headers e2)
-    | Cifthenelse (e1, e2, e3) ->
-      Cifthenelse (instrument_headers e1, instrument_headers e2,
-        instrument_headers e3)
-    | Cswitch (e, is, es) ->
-      Cswitch (instrument_headers e, is, Array.map instrument_headers es)
-    | Cloop e ->
-      Cloop (instrument_headers e)
-    | Ccatch (n, ids, e1, e2) ->
-      Ccatch (n, ids, instrument_headers e1, instrument_headers e2)
-    | Cexit (n, es) ->
-      Cexit (n, List.map instrument_headers es)
-    | Ctrywith (e1, id, e2) ->
-      Ctrywith (instrument_headers e1, id, instrument_headers e2)
-    | Ctailrec_entry_point -> Ctailrec_entry_point
+  let callee_addr =
+    Cop (Cor, [Cconst_symbol callee; Cconst_int 3])
   in
-  let expr = instrument_headers expr in
-  !next_alloc_point_number, !contains_calls, expr
-
-let fundecl decl =
-  if not !Clflags.allocation_profiling then
-    { decl with
-      fun_body = Csequence (Ctailrec_entry_point, decl.fun_body);
-    }
-  else
-    let backtrace_bucket = Ident.create "backtrace_bucket" in
-    let num_allocation_points, contains_calls, body =
-      instrument_function_body decl.fun_body ~backtrace_bucket
-        ~fun_name:decl.fun_name
-    in
-    (* Even if there are no allocation points, we must record a backtrace
-       stack frame unless the function is a leaf function. *)
-    if num_allocation_points < 1 && (not contains_calls) then decl
-    else
-      let body =
-        add_prologue ~body ~num_allocation_points ~backtrace_bucket
-      in
-      { decl with
-        fun_body = body;
-        fun_num_instrumented_alloc_points = num_allocation_points;
-      }
+  let node_hole_ptr =
+    Cop (Caddi, [place_within_node; Cconst_int Arch.size_addr])
+  in
+  Csequence (
+    Cop (Cstore Word, [place_within_node; callee_addr]),
+    Cop (Calloc_profiling_load_node_hole_ptr, node_hole_ptr))
