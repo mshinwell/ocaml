f3ff42e7dae848281faf378442296a7c28f6b993
Mark Shinwell <mshinwell@janestreet.com>
2016-02-05 12:27:43 +0000

    the new world

--- middle_end/unbox_closures.ml
+++ middle_end/unbox_closures.ml
@@ -27,20 +27,24 @@ module Transform = struct
       && (Variable.Map.is_empty set_of_closures.specialised_args
         || Flambda_utils.contains_stub set_of_closures.function_decls)
 
-  let what_to_specialise ~env:_ ~set_of_closures =
+  let what_to_specialise ~env:_ ~(set_of_closures : Flambda.set_of_closures) =
     let what_to_specialise = W.create ~set_of_closures in
     if not (precondition ~set_of_closures) then
       what_to_specialise
     else
       Flambda_iterators.fold_function_decls_ignoring_stubs set_of_closures
         ~init:what_to_specialise
-        ~f:(fun ~fun_var ~(function_decl : Flambda.function_declaration)
-              what_to_specialise ->
+        ~f:(fun ~fun_var ~function_decl:_ what_to_specialise ->
+          let bound_by_the_closure =
+            Flambda_utils.variables_bound_by_the_closure
+              (Closure_id.wrap fun_var)
+              set_of_closures.function_decls
+          in
           Variable.Set.fold (fun inner_free_var what_to_specialise ->
               W.new_specialised_arg what_to_specialise
                 ~fun_var ~group:inner_free_var
                 ~definition:(Existing_inner_free_var inner_free_var))
-            function_decl.free_variables
+            bound_by_the_closure
             what_to_specialise)
 end
 
