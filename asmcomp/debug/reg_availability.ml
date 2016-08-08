24ec97bae50f683df9dce6a2af88a9d7fc6582d6
Mark Shinwell <mshinwell@janestreet.com>
2016-08-08 12:42:59 +0100

    debugging

--- asmcomp/debug/reg_availability.ml
+++ asmcomp/debug/reg_availability.ml
@@ -12,6 +12,8 @@
 (*                                                                        *)
 (**************************************************************************)
 
+[@@@ocaml.warning "+a-4-9-30-40-41-42"]
+
 module RD = Reg_with_debug_info
 
 type t =
@@ -26,32 +28,41 @@ let inter regs1 regs2 =
     Ok (RD.Set.inter avail1 avail2)
 
 let canonicalise availability =
-  let regs_by_ident = Ident.Tbl.create 42 in
-  RD.Set.iter (fun reg ->
-      if RD.has_debug_info reg then begin
-        if not (Ident.persistent name) then begin
-          match Ident.Tbl.find regs_by_ident name with
-          | exception Not_found -> Ident.Tbl.add regs_by_ident name reg
-          | (reg' : RD.t) ->
-            (* We prefer registers that are assigned to the stack to
-               preserve availability across function calls.  Other than
-               that, any register is as good as any other register; likewise
-               for stack slots. *)
-            (* CR mshinwell: think about this again.  Does it matter what's
-               chosen here?  May be better to choose "Reg" as the dwarf is
-               probably smaller *)
-            match RD.loc reg, RD.loc reg' with
-            | Reg _, Stack _
-            | Reg _, Reg _
-            | Stack _, Stack _
-            | _, Unknown
-            | Unknown, _ -> ()
-            | Stack _, Reg _ ->
-              Ident.Tbl.remove regs_by_ident name;
-              Ident.Tbl.add regs_by_ident name reg
-        end
-      end)
-    availability
-  Ident.Tbl.fold (fun _ident reg availability -> Reg.Set.add reg availability)
-    regs_by_ident
-    Reg.Set.empty
+  match availability with
+  | Unreachable -> Unreachable
+  | Ok availability ->
+    let regs_by_ident = Ident.Tbl.create 42 in
+    RD.Set.iter (fun reg ->
+        match RD.debug_info reg with
+        | None -> ()
+        | Some debug_info ->
+          let name = RD.Debug_info.holds_value_of debug_info in
+          if not (Ident.persistent name) then begin
+            match Ident.Tbl.find regs_by_ident name with
+            | exception Not_found -> Ident.Tbl.add regs_by_ident name reg
+            | (reg' : RD.t) ->
+              (* We prefer registers that are assigned to the stack to
+                preserve availability across function calls.  Other than
+                that, any register is as good as any other register; likewise
+                for stack slots. *)
+              (* CR mshinwell: think about this again.  Does it matter what's
+                chosen here?  May be better to choose "Reg" as the dwarf is
+                probably smaller *)
+              match RD.location reg, RD.location reg' with
+              | Reg _, Stack _
+              | Reg _, Reg _
+              | Stack _, Stack _
+              | _, Unknown
+              | Unknown, _ -> ()
+              | Stack _, Reg _ ->
+                Ident.Tbl.remove regs_by_ident name;
+                Ident.Tbl.add regs_by_ident name reg
+          end)
+      availability;
+    let result =
+      Ident.Tbl.fold (fun _ident reg availability ->
+          RD.Set.add reg availability)
+        regs_by_ident
+        RD.Set.empty
+    in
+    Ok result
