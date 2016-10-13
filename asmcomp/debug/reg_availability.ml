a8e30a597a8a7db2165605716733b38141b16cc7
Mark Shinwell <mshinwell@janestreet.com>
2016-10-13 10:27:18 +0100

    fix

--- asmcomp/debug/reg_availability.ml
+++ asmcomp/debug/reg_availability.ml
@@ -79,13 +79,9 @@ let canonicalise availability =
             match Ident.Tbl.find regs_by_ident name with
             | exception Not_found -> Ident.Tbl.add regs_by_ident name reg
             | (reg' : RD.t) ->
-              (* We prefer registers that are assigned to the stack to
-                 preserve availability across function calls.  Other than
-                 that, any register is as good as any other register; likewise
-                 for stack slots. *)
-              (* CR mshinwell: think about this again.  Does it matter what's
-                 chosen here?  May only be the DWARF size that it affects
-                 ("reg" probably smaller, but fewer "stack" operators) *)
+              (* We prefer registers that are assigned to the stack since
+                 they probably give longer available ranges (less likely to
+                 be clobbered). *)
               match RD.location reg, RD.location reg' with
               | Reg _, Stack _
               | Reg _, Reg _
