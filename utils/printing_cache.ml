dd1aabed4fe7570dc7a910908a1c166f38539c92
Mark Shinwell <mshinwell@gmail.com>
2018-03-07 13:17:31 +0000

    Printing_cache

---
+++ utils/printing_cache.ml
@@ -0,0 +1,42 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                  Mark Shinwell, Jane Street Europe                     *)
+(*                                                                        *)
+(*   Copyright 2018 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.warning "+a-4-30-40-41-42"]
+
+type seen = S : _ * string -> seen
+
+type 'a t = {
+  prefix : string;
+  mutable next_id : int;
+  mutable printed : seen list;
+}
+
+let create prefix =
+  { prefix;
+    next_id = 0;
+    printed = [];
+  }
+
+let with_cache t ppf obj printer =
+  let rec find = function
+    | (S (name, obj'))::printed ->
+      if obj == obj' then Format.fprintf ppf "*%s" name
+      else find printed
+    | [] ->
+      let name = Printf.sprintf "%s%d" t.prefix t.next_id in
+      t.next_id <- t.next_id + 1;
+      t.printed <- (S (name obj)) :: t.printed;
+      Format.fprintf ppf "@[&%s =@ %a@]" printer ()
+  in
+  find t.printed
---
+++ utils/printing_cache.mli
@@ -0,0 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                  Mark Shinwell, Jane Street Europe                     *)
+(*                                                                        *)
+(*   Copyright 2018 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.warning "+a-4-30-40-41-42"]
+
+type 'a t
+
+val create : string -> 'a t
+
+val with_cache : 'a t -> 'a -> (unit -> unit) -> unit
