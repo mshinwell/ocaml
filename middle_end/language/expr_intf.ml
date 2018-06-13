f6fdbc1d1d6889a844e6e108ada2680952ce9ab9
Mark Shinwell <mshinwell@gmail.com>
2018-06-13 13:49:47 +0100

    Meet/join

---
+++ middle_end/language/expr_intf.ml
@@ -0,0 +1,32 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2013--2018 OCamlPro SAS                                    *)
+(*   Copyright 2014--2018 Jane Street Group LLC                           *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.warning "+a-4-9-30-40-41-42"]
+
+module type S = sig
+  type t
+  type flambda_type
+
+  val print_with_cache : cache:Printing_cache.t -> Format.formatter -> t -> unit
+
+  val free_names : t -> Name_occurrences.t
+
+  val equal
+     : equal_type:(flambda_type -> flambda_type -> bool)
+    -> t
+    -> t
+    -> bool
+end
