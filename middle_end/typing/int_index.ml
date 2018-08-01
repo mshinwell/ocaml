72f96c484caa326da76e2537ac53c83011d387ea
Mark Shinwell <mshinwell@gmail.com>
2018-08-01 11:32:08 +0100

    typing

---
+++ middle_end/typing/int_index.ml
@@ -0,0 +1,25 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2018 OCamlPro SAS                                          *)
+(*   Copyright 2018 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.warning "+a-4-30-40-41-42"]
+
+include Targetint.OCaml
+
+let free_names _t = Name_occurrences.create ()
+let bound_names _t = Name_occurrences.create ()
+
+let apply_name_permutation t _perm = t
+let freshen t _freshening = t
