b1c776a9eb352ac531352d20b5d93fbe4888180b
Mark Shinwell <mshinwell@gmail.com>
2018-08-09 12:21:17 +0100

    typing

--- middle_end/typing/type_equality.mli
+++ middle_end/typing/type_equality.mli
@@ -16,26 +16,7 @@
 
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
-module Make_types
-  (T : Typing_world_abstract.S)
-  (Functor_T : Typing_world_abstract.Functor_S)
-  : Type_equality_intf.S_types
-      with module T := T
-      and module Functor_T := Functor_T
-
-module type Strengthened_world = sig
-  module Recursive_world : sig
-    module rec Types : (Typing_world_types.Types_nonrec
-      with module Abstract_types := Types
-      and module Abstract_functor_types := Functor_types
-      with module Type_equality = Make_types (Types) (Functor_types))
-    and Functor_types : Typing_world_types.Functor_types_nonrec
-      with module Abstract_types := Types
-  end
-  include Typing_world.S with module Typing_world := Recursive_world
-end
-
-module Make (W : Strengthened_world) (F : Typing_world.Functor_S)
+module Make (W : Typing_world.S) (F : Typing_world.Functor_S)
   : Type_equality_intf.S
-      with module T := W.Recursive_world.Types
-      and module Functor_T := W.Recursive_world.Functor_types
+      with module T := W.Typing_world.Types
+      and module Functor_T := W.Typing_world.Functor_types
