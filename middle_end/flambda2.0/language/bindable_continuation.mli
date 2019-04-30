ff2499537dbe5e7534452b7e66c2bb4dc3886655
Mark Shinwell <mshinwell@gmail.com>
2019-04-30 14:39:53 +0100

    closure_conversion

--- middle_end/flambda2.0/language/bindable_continuation.mli
+++ middle_end/flambda2.0/language/bindable_continuation.mli
@@ -14,39 +14,11 @@
 (*                                                                        *)
 (**************************************************************************)
 
+(** A [Continuation] equipped with operations that mean it can be used in
+    binding position within a [Name_abstraction] value. *)
+
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
-module T0 = struct
-  type t =
-    | Continuation of Continuation.t
-    | Name of Name.t
+type t = Continuation.t
 
-  let compare t1 t2 =
-    match t1, t2 with
-    | Continuation k1, Continuation k2 -> Continuation.compare k1 k2
-    | Name name1, Name name2 -> Name.compare name1 name2
-    | Name _, Continuation _ -> 1
-    | Continuation _, Name _ -> -1
-
-  let equal t1 t2 = (compare t1 t2 = 0)
-
-  let hash t =
-    match t with
-    | Continuation k -> Hashtbl.hash (0, Continuation.hash k)
-    | Name name -> Hashtbl.hash (1, Name.hash name)
-
-  let print ppf t =
-    match t with
-    | Continuation k -> Continuation.print ppf k
-    | Name name -> Name.print ppf name
-
-  let output chan t =
-    print (Format.formatter_of_out_channel chan) t
-end
-
-include T0
-include Identifiable.Make (T0)
-
-let rename = function
-  | Continuation _ -> Continuation (Continuation.create ())
-  | Name name -> Name (Name.rename name)
+include Bindable.S with type t := t
--- middle_end/flambda2.0/language/bindable_name.mli
+++ middle_end/flambda2.0/language/bindable_name.mli
@@ -5,8 +5,8 @@
 (*                       Pierre Chambart, OCamlPro                        *)
 (*           Mark Shinwell and Leo White, Jane Street Europe              *)
 (*                                                                        *)
-(*   Copyright 2018 OCamlPro SAS                                          *)
-(*   Copyright 2018 Jane Street Group LLC                                 *)
+(*   Copyright 2019 OCamlPro SAS                                          *)
+(*   Copyright 2019 Jane Street Group LLC                                 *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -14,14 +14,11 @@
 (*                                                                        *)
 (**************************************************************************)
 
-(** All of the different varieties of names that may be bound. *)
+(** A [Name] equipped with operations that mean it can be used in binding
+    position within a [Name_abstraction] value. *)
 
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
-type t =
-  | Continuation of Continuation.t
-  | Name of Name.t
+type t = Name.t
 
-include Identifiable.S with type t := t
-
-val rename : t -> t
+include Bindable.S with type t := t
