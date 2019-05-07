0e96cf9568f13b1ad26bd607f32afb5a7ae6a736
Mark Shinwell <mshinwell@gmail.com>
2019-05-07 12:49:27 +0100

    typing

--- middle_end/flambda2.0/typing/structures/trivial_row_like.mli
+++ middle_end/flambda2.0/typing/structures/trivial_row_like.mli
@@ -1,48 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2013--2019 OCamlPro SAS                                    *)
+(*   Copyright 2014--2019 Jane Street Group LLC                           *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
 
-module Make (Thing_without_names : Hashtbl.With_map) : sig
-  type t
+[@@@ocaml.warning "+a-4-30-40-41-42"]
 
-  val print : cache:Printing_cache.t -> Format.formatter -> t -> unit
-
-  (** Create a value which describes the presence of exactly no things. *)
-  val create_bottom : unit -> t
-
-  (** Create a value which describes the presence of an unknown set of
-      things. *)
-  val create_unknown : unit -> t
-
-  val create : Thing_without_names.Set.t -> t
-
-  val create_with_equations
-     : Typing_env_extension.t Thing_without_names.Map.t
-    -> t
-
-  val equal
-     : Type_equality_env.t
-    -> Type_equality_result.t
-    -> t
-    -> t
-    -> Type_equality_result.t
-
-  val meet
-     : Meet_env.t
-    -> t
-    -> t
-    -> (t * Typing_env_extension.t) Or_bottom.t
-
-  val join
-     : Join_env.t
-    -> t
-    -> t
-    -> t
-
-  val all : t -> Thing_without_names.Set.t Or_unknown.t
-
-  val get_singleton
-     : t
-    -> (Thing_without_names.t * Typing_env_extension.t) option
-
-  val classify : t -> unit Or_unknown_or_bottom.t
-
-  include Contains_names.S with type t := t
-end
+module Make (Thing_without_names : Identifiable.S) :
+  Trivial_row_like_intf.S
+    with module Thing_without_names := Thing_without_names
