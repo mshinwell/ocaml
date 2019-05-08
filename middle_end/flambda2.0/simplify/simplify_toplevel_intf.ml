fdda989e3626674e402ff0b7130dba3d601ab0c4
Mark Shinwell <mshinwell@gmail.com>
2019-05-08 17:15:42 +0100

    simplify

--- middle_end/flambda2.0/simplify/simplify_toplevel_intf.ml
+++ middle_end/flambda2.0/simplify/simplify_toplevel_intf.ml
@@ -5,8 +5,8 @@
 (*                       Pierre Chambart, OCamlPro                        *)
 (*           Mark Shinwell and Leo White, Jane Street Europe              *)
 (*                                                                        *)
-(*   Copyright 2013--2018 OCamlPro SAS                                    *)
-(*   Copyright 2014--2018 Jane Street Group LLC                           *)
+(*   Copyright 2013--2019 OCamlPro SAS                                    *)
+(*   Copyright 2014--2019 Jane Street Group LLC                           *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -19,7 +19,6 @@
 module type S = sig
   val simplify_toplevel
      : Simplify_env_and_result.Env.t
-    -> Simplify_env_and_result.Result.t
     -> Flambda.Expr.t
     -> continuation:Continuation.t
     -> continuation_params:Kinded_parameter.t list
@@ -27,7 +26,6 @@ module type S = sig
     -> descr:string
     -> scope_level_for_lifted_constants:Scope_level.t
     -> Flambda.Expr.t * Simplify_env_and_result.Result.t
-         * Continuation_uses.t
          * (Flambda_type.t * Flambda_kind.t * Flambda_static.Static_part.t)
              Symbol.Map.t
 end
