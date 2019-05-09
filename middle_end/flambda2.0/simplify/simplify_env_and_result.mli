27fd1fc2d5a27f8c4c8370c4ab9569dc8939ea29
Mark Shinwell <mshinwell@gmail.com>
2019-05-09 14:19:23 +0100

    simplify

--- middle_end/flambda2.0/simplify/simplify_env_and_result.mli
+++ middle_end/flambda2.0/simplify/simplify_env_and_result.mli
@@ -5,8 +5,8 @@
 (*                       Pierre Chambart, OCamlPro                        *)
 (*           Mark Shinwell and Leo White, Jane Street Europe              *)
 (*                                                                        *)
-(*   Copyright 2013--2017 OCamlPro SAS                                    *)
-(*   Copyright 2014--2017 Jane Street Group LLC                           *)
+(*   Copyright 2013--2019 OCamlPro SAS                                    *)
+(*   Copyright 2014--2019 Jane Street Group LLC                           *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -16,7 +16,7 @@
 
 (** Environments and result structures used during simplification. *)
 
-[@@@ocaml.warning "+a-4-9-30-40-41-42"]
+[@@@ocaml.warning "+a-4-30-40-41-42"]
 
 module rec Env : sig
   include Simplify_env_and_result_intf.Env
