8f04e5d153c9ccd75a3f95650a4c44e3808baa0a
Mark Shinwell <mshinwell@gmail.com>
2019-05-13 14:38:15 +0100

    typing

--- middle_end/flambda2.0/typing/derived_structures/blocks.rec.ml
+++ middle_end/flambda2.0/typing/derived_structures/blocks.rec.ml
@@ -16,41 +16,21 @@
 
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
-module Tag_and_targetint_ocaml =
-  Hashtbl.Make_with_map_pair (Tag) (Targetint.OCaml)
+module Tag_and_targetint_ocaml = struct
+  type t = Tag.t * Targetint.OCaml.t
+  include Identifiable.Make_pair (Tag) (Targetint.OCaml)
+end
 
 module RL =
   Row_like.Make (Tag) (Targetint.OCaml) (Tag_and_targetint_ocaml) (Product)
 
-type t = RL.t
+include RL
 
 type open_or_closed = Open | Closed of Tag.t
 
-let create ~field_tys open_or_closed : t =
-  let product = Product.create field_tys in
+let create ~field_tys kind open_or_closed : t =
+  let product = Product.create field_tys kind in
   let size = Targetint.OCaml.of_int (List.length field_tys) in
   match open_or_closed with
-  | Open -> RL.create_at_least size product
-  | Closed tag -> RL.create_exactly tag size product
-
-let create_bottom = RL.create_bottom 
-
-let _invariant _t = () (* CR mshinwell: RL.invariant *)
-
-let print_with_cache = RL.print
-
-let equal = RL.equal
-let is_empty = RL.is_bottom
-
-let meet env t1 t2 : _ Or_bottom.t =
-  match RL.meet env t1 t2 with
-  | Bottom -> Bottom
-  | Ok (t, product) ->
-    Ok (t, RP.standalone_extension product)
-
-let join = RL.join
-
-let free_names = RL.free_names
-let apply_name_permutation = RL.apply_name_permutation
-
-let classify = RL.classify
+  | Open -> create_at_least size product
+  | Closed tag -> create_exactly tag size product
