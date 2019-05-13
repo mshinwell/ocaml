6083bd99016b300aa526128d3ad4d83f8fd1b313
Mark Shinwell <mshinwell@gmail.com>
2019-05-13 14:38:15 +0100

    typing

--- middle_end/flambda2.0/typing/derived_structures/types_by_closure_id.rec.ml
+++ middle_end/flambda2.0/typing/derived_structures/types_by_closure_id.rec.ml
@@ -21,20 +21,4 @@ type t = Flambda_types.t Closure_id.Map.t
 (* CR mshinwell: Any line of the following form should be removed *)
 let print = print_with_cache
 
-let create closure_ids_to_tys =
-  let closure_ids_to_logical_variables =
-    Closure_id.Map.map (fun _ty ->
-        Logical_variable.create (Flambda_kind.value ()))
-      closure_ids_to_tys
-  in
-  let env_extension =
-    Closure_id.Map.fold (fun closure_id ty env_extension ->
-        let logical_var =
-          Closure_id.Map.find closure_id closure_ids_to_logical_variables
-        in
-        Typing_env_extension.add_equation env_extension
-          (Name.logical_var logical_var) ty)
-      closure_ids_to_tys
-      (Typing_env_extension.empty ())
-  in
-  create closure_ids_to_logical_variables env_extension
+let create closure_ids_to_tys = closure_ids_to_tys
