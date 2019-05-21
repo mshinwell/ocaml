2ef3adf9805275d7b3ed9697ce356936b87e1c6c
Mark Shinwell <mshinwell@gmail.com>
2019-05-21 14:53:33 +0100

    products

--- middle_end/flambda2.0/typing/structures/product.rec.ml
+++ middle_end/flambda2.0/typing/structures/product.rec.ml
@@ -15,45 +15,82 @@
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
 module K = Flambda_kind
+module T = Flambda_types
 
-type t = {
-  kind : K.t;
-  tys : Flambda_types.t list;
-}
+module Make (Index : Name_like_intf.S) = struct
+  type t = {
+    components_by_index : T.t Index.Map.t;
+  }
 
-let create tys kind =
-  let kinds = List.map Flambda_type0_core.kind tys in
-  match kinds with
-  | [] ->
-    { kind;
-      ty;
-    }
-  | kinds ->
-    let kind_set = K.Set.of_list kinds in
-    match K.Set.get_singleton kind_set with
-    | Some kind' ->
-      if not (K.equal kind kind') then begin
-        Misc.fatal_errorf "[kind] argument (%a) to [Product.create] \
-            doesn't match kind(s) of supplied [tys] (%a)"
-          K.print kind
-          K.print kind'
-      end;
-      { tys;
-        kind;
-      }
-    | None ->
-      Misc.fatal_error "Supplied types to [Product.create] differ: %a"
-        K.Set.print kind_set
+  let invariant t =
+    Index.Map.cardinal t.components_by_index > 0
 
-let create_bottom ~arity kind =
-  let tys = List.init arity (fun _ -> Flambda_type0_core.bottom kind) in
-  create tys kind
+  let print ppf { components_by_index; } =
+    Format.fprintf ppf
+      "@[<hov 1>(\
+        @[<hov 1>(components_by_index@ %a)@]\
+        )@]"
+      (Index.Map.print Type_printers.print) components_by_index
 
-let widen t ~to_match =
-  let t_length = List.length t in
-  let to_match_length = List.length to_match in
-  if t_length >= to_match_length then t
-  else
-    let extra_length = to_match_length - t_length in
-    let extra = create_bottom ~arity:extra_length t.kind in
-    create (t.tys @ extra.tys) t.kind
+  let print_with_cache ~cache:_ ppf t = print ppf t
+
+  let equal env result
+        { components_by_index = components_by_index1; }
+        { components_by_index = components_by_index2; } =
+    let equal =
+      Index.Map.fold2_stop_on_key_mismatch
+        (fun _index component1 component2 equal ->
+          equal && Type_equality.equal env env component1 component2)
+        components_by_index1 components_by_index2
+        true
+    in
+    match equal with
+    | None | Some false -> false
+    | Some true -> true
+
+  let indexes t = Index.Map.keys t.components_by_index
+
+  let meet env
+        { components_by_index = components_by_index1; }
+        { components_by_index = components_by_index2; } : _ Or_bottom.t =
+    let all_bottom = ref true in
+    let components_by_index =
+      Index.Map.merge (fun ty1_opt ty2_opt ->
+          match ty1_opt, ty2_opt with
+          | None, None | Some _, None | None, Some _ -> None
+          | Some ty1, Some ty2 ->
+            match T.meet env ty1 ty2 with
+            | Ok ty ->
+              all_bottom := false;
+              Some ty
+            | Bottom -> Some (T.bottom K.value))
+        components_by_index1
+        components_by_index2
+    in
+    if !all_bottom then Bottom
+    else Ok { components_by_index; }
+
+  let join env
+        { components_by_index = components_by_index1; }
+        { components_by_index = components_by_index2; } =
+    let components_by_index =
+      Index.Map.merge (fun ty1_opt ty2_opt ->
+          match ty1_opt, ty2_opt with
+          | None, None -> None
+          | Some ty, None | None, Some ty -> Some ty
+          | Some ty1, Some ty2 -> Some (T.join env ty1 ty2))
+        components_by_index1
+        components_by_index2
+    in
+    { components_by_index; }
+
+  let widen t ~to_match =
+    let missing_indexes = Index.Set.diff (indexes to_match) (indexes t) in
+    let components_by_index =
+      Index.Set.fold (fun index components_by_index ->
+          Index.Map.add index (T.any_value ()) components_by_index)
+        missing_indexes
+        t.components_by_index
+    in
+    { t with components_by_index; }
+end
--- middle_end/flambda2.0/typing/structures/product.rec.mli
+++ middle_end/flambda2.0/typing/structures/product.rec.mli
@@ -12,35 +12,25 @@
 (*                                                                        *)
 (**************************************************************************)
 
-(** Straightforward ordered product, e.g. for representing types of block fields
-    without tracking any relations between them.
-
-    Projections from products are achieved using [meet] operations.
-*)
-
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
-type t
+module Make (Index : Name_like_intf.S) : sig
+  type t
 
-(** Create a product given the kind of its elements and the types of its
-    elements.  The kinds of all types in the list must be the same and equal
-    to the supplied kind. *)
-val create : Flambda_types.t list -> Flambda_kind.t -> t
+  (** Create a product value given the indexes with associated components. *)
+  val create : Flambda_types.t Index.Map.t -> t
 
-(** Create a product with the given number of fields of the given kind.
-    The types of the fields will be set to bottom. *)
-val create_bottom : arity:int -> Flambda_kind.t -> t
+  (** Widen the product by adding as many fields, after any existing fields,
+      so that the product has the same number of fields as [to_match].  If the
+      supplied product already has at least that many fields then it is
+      returned unchanged. *)
+  val widen : t -> to_match:t -> t
 
-(** Widen the product by adding as many fields, after any existing fields,
-    so that the product has the same number of fields as [to_match].  If the
-    supplied product already has at least that many fields then it is
-    returned unchanged. *)
-val widen : t -> to_match:t -> t
-
-include Type_structure_intf.S
-  with type t := t
-  with type join_env := Join_env.t
-  with type meet_env := Meet_env.t
-  with type type_equality_env := Type_equality_env.t
-  with type type_equality_result := Type_equality_result.t
-  with type typing_env_extension := Typing_env_extension.t
+  include Type_structure_intf.S
+    with type t := t
+    with type join_env := Join_env.t
+    with type meet_env := Meet_env.t
+    with type type_equality_env := Type_equality_env.t
+    with type type_equality_result := Type_equality_result.t
+    with type typing_env_extension := Typing_env_extension.t
+end
