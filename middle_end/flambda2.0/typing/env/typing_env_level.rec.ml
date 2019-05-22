b0cba825376d93b4eaf3e21e636909cf7fbbfd8e
Mark Shinwell <mshinwell@gmail.com>
2019-05-22 17:16:45 +0100

    env

--- middle_end/flambda2.0/typing/env/typing_env_level.rec.ml
+++ middle_end/flambda2.0/typing/env/typing_env_level.rec.ml
@@ -207,10 +207,6 @@ let meet env (t1 : t) (t2 : t) : t =
   end else if is_empty t2 then begin
     t1
   end else begin
-Format.eprintf "Typing_env_level.meet@ %a@ and@ %a@ in env@ %a\n%!" print t1 print t2
-Meet_env.print env;
-    let t1 = apply_name_permutation t1 (Meet_env.perm_left env) in
-    let t2 = apply_name_permutation t2 (Meet_env.perm_right env) in
     let env = Meet_env.env env in
     let env = Typing_env.increment_scope_level env in
     (* The domains of the levels are treated as contravariant.  As such,
