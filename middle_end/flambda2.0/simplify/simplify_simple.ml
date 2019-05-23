f5a880676ad34cb7c40e7fe3f61f7ab8ee0069c0
Mark Shinwell <mshinwell@gmail.com>
2019-05-23 16:34:33 +0100

    typing

--- middle_end/flambda2.0/simplify/simplify_simple.ml
+++ middle_end/flambda2.0/simplify/simplify_simple.ml
@@ -20,50 +20,18 @@ module E = Simplify_env_and_result.Env
 module T = Flambda_type
 module TE = T.Typing_env
 
-(* CR mshinwell: Share with [Typing_env] *)
-let type_for_const (const : Simple.Const.t) =
-  match const with
-  | Naked_immediate i -> T.this_naked_immediate i
-  | Tagged_immediate i -> T.this_tagged_immediate i
-  | Naked_float f -> T.this_naked_float f
-  | Naked_int32 n -> T.this_naked_int32 n
-  | Naked_int64 n -> T.this_naked_int64 n
-  | Naked_nativeint n -> T.this_naked_nativeint n
-
-let simplify_name_for_let env r name =
-  let typing_env = E.typing_env env in
-  let ty = TE.find_exn typing_env name in
-  let ty, canonical_simple = TE.resolve_aliases typing_env ty in
-  let simple =
-    match canonical_simple with
-    | None -> Simple.name name
-    | Some canonical_simple -> canonical_simple
-  in
-  simple, T.alias_type_of (T.kind ty) simple, r
-
-let simplify_simple_for_let env r (simple : Simple.t) =
-  match simple with
-  | Const c -> simple, type_for_const c, r
-  | Discriminant t -> simple, T.this_discriminant t, r
-  | Name name -> simplify_name_for_let env r name
-
 let simplify_name env (name : Name.t) =
   let typing_env = E.typing_env env in
   let ty = TE.find_exn typing_env name in
-(* Experiment: don't reify so that we preserve relations *)
-  name, T.alias_type_of (T.kind ty) (Simple.name name)
-(* The following works.  Maybe we could do this only for names which were
-   syntactically bound to constants.
-    let reified =
-      T.reify ~allow_free_variables:true
-        (E.get_typing_environment env)
-        ty
-    in
-    match reified with
-    | Term (simple, ty) -> simple, ty
-    | Cannot_reify | Lift _ -> Simple.name name, ty
-    | Invalid -> Simple.name name, T.bottom_like ty
-*)
+  let reified =
+    T.reify ~allow_free_variables:true
+      (E.get_typing_environment env)
+      ty
+  in
+  match reified with
+  | Term (simple, ty) -> simple, ty
+  | Cannot_reify | Lift _ -> Simple.name name, ty
+  | Invalid -> Simple.name name, T.bottom_like ty
 
 let simplify_simple env (simple : Simple.t) =
   match simple with
