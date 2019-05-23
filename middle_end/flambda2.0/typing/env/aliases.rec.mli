21dd8b61caf369b83b7d642545a2c65e3625cd74
Mark Shinwell <mshinwell@gmail.com>
2019-05-23 11:26:24 +0100

    aliases

--- middle_end/flambda2.0/typing/env/aliases.rec.mli
+++ middle_end/flambda2.0/typing/env/aliases.rec.mli
@@ -14,44 +14,7 @@
 (*                                                                        *)
 (**************************************************************************)
 
-[@@@ocaml.warning "+a-4-9-30-40-41-42"]
+[@@@ocaml.warning "+a-4-30-40-41-42"]
 
-type t = private
-  | Var of Variable.t
-  | Symbol of Symbol.t
-(* CR mshinwell: Phantom variables should be in here now.
-  | Phantom_var of Variable.t
-*)
+type t
 
-val var : Variable.t -> t
-val symbol : Symbol.t -> t
-
-val map_var : t -> f:(Variable.t -> Variable.t) -> t
-
-val map_symbol : t -> f:(Symbol.t -> Symbol.t) -> t
-
-val to_var : t -> Variable.t option
-
-include Identifiable.S with type t := t
-
-val print_sexp : Format.formatter -> t -> unit
-
-val variables_only : Set.t -> Set.t
-
-val symbols_only_map : 'a Map.t -> 'a Map.t
-
-val set_to_var_set : Set.t -> Variable.Set.t
-
-val set_to_symbol_set : Set.t -> Symbol.Set.t
-
-val is_predefined_exception : t -> bool
-
-val rename : t -> t
-
-val in_compilation_unit : t -> Compilation_unit.t -> bool
-
-module Pair : sig
-  type nonrec t = t * t
-
-  include Identifiable.S with type t := t
-end
