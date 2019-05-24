96ebb2c838d67bff8643041b079ff465e70eeeee
Mark Shinwell <mshinwell@gmail.com>
2019-05-24 10:23:32 +0100

    typing

--- middle_end/flambda2.0/simplify/simplify_simple.ml
+++ middle_end/flambda2.0/simplify/simplify_simple.ml
@@ -20,26 +20,25 @@ module E = Simplify_env_and_result.Env
 module T = Flambda_type
 module TE = T.Typing_env
 
-let simplify_name env (name : Name.t) =
+let simplify_name env name =
   let typing_env = E.typing_env env in
-  let ty = TE.find_exn typing_env name in
-  let reified =
-    T.reify ~allow_free_variables:true
-      (E.get_typing_environment env)
-      ty
-  in
-  match reified with
-  | Term (simple, ty) -> simple, ty
-  | Cannot_reify | Lift _ -> Simple.name name, ty
-  | Invalid -> Simple.name name, T.bottom_like ty
+  let name = TE.get_canonical_name typing_env name in
+  (* CR mshinwell: Avoid double lookup here *)
+  let ty = TE.find typing_env name in
+  name, ty
 
 let simplify_simple env (simple : Simple.t) =
   match simple with
-  | Const c -> simple, type_for_const c
+  | Const c -> simple, T.type_for_const c
   | Discriminant t -> simple, T.this_discriminant t
   | Name name ->
-    let name, ty = simplify_name env name in
-    Simple.name name, ty
+    let typing_env = E.typing_env env in
+    let ty = TE.find typing_env name in
+    let reified = T.reify ~allow_free_variables:true typing_env ty in
+    match reified with
+    | Term (simple, ty) -> simple, ty
+    | Cannot_reify | Lift _ -> Simple.name name, ty
+    | Invalid -> Simple.name name, T.bottom_like ty
 
 let simplify_simple_and_drop_type env simple =
   fst (simplify_simple env simple)
