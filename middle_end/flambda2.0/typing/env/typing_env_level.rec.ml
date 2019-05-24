bd118f4b0dd9658c23a37a38886e14a6a233a10c
Mark Shinwell <mshinwell@gmail.com>
2019-05-24 10:08:56 +0100

    typing

--- middle_end/flambda2.0/typing/env/typing_env_level.rec.ml
+++ middle_end/flambda2.0/typing/env/typing_env_level.rec.ml
@@ -21,8 +21,7 @@ type t = {
   equations : Flambda_types.t Name.Map.t;
 }
 
-let print_with_cache ~cache ppf
-      ({ defined_names; equations; } : t) =
+let print_with_cache ~cache ppf ({ defined_names; equations; } : t) =
   let print_equations ppf equations =
     let equations = Name.Map.bindings equations in
     match equations with
@@ -52,12 +51,7 @@ let print_with_cache ~cache ppf
           @[<hov 1>(defined_names@ @[<v 1>%a@])@]@;\
           @[<hov 1>(equations@ @[<v 1>%a@])@]\
           )@]"
-      (* CR mshinwell: Fix this.  The problem is that Logical_variable prints
-         the types *)
       Name.Set.print (Name.Map.keys defined_names)
-(*
-      (Name.Map.print Flambda_kind.print) defined_names
-*)
       print_equations equations
 
 let print ppf t =
@@ -76,125 +70,17 @@ let is_empty { defined_names; equations; } =
 
 let equations t = t.equations
 
-let equations_domain t = Name.Map.keys t.equations
-
-let equations_on_outer_env_domain t =
-  Name.Set.diff (equations_domain t) (Name.Map.keys t.defined_names)
-
-let restrict_to_names { defined_names; equations; } allowed_names =
-  let allowed_names =
-    Name_occurrences.everything_must_only_be_names allowed_names
-  in
-  let defined_names =
-    Name.Map.filter (fun name _kind -> Name.Set.mem name allowed_names)
-      defined_names
-  in
-  let equations =
-    Name.Map.filter (fun name _ty -> Name.Set.mem name allowed_names)
-      equations
-  in
-  let t =
-    { defined_names;
-      equations;
-    }
-  in
-  invariant t;
-  t
-
-let find_equation_opt t name =
-  match Name.Map.find name t.equations with
-  | exception Not_found -> None
-  | ty -> Some ty
-
-let find_opt t name =
-  match find_equation_opt t name with
-  | None ->
-    begin match Name.Map.find name t.defined_names with
-    | exception Not_found -> None
-    | kind -> Some (Flambda_type0_core.unknown kind)
-    end
-  | some_ty -> some_ty
-
-let find_exn t name =
-  match find_opt t name with
-  | Some ty -> ty
-  | None ->
-    Misc.fatal_errorf "Unbound name %a in@ %a"
-      Name.print name
-      print t
-
-let tidy t =
-  let free_names_minus_defined_names' =
-    free_names_minus_defined_names t
-  in
-  (* XXX *)
-  let defined_names = free_names_in_defined_names t in
-  let equations =
-    Name.Map.map_sharing (fun ty ->
-        let rec resolve_aliases ty =
-          (* CR mshinwell: Needs check for cycles *)
-          match Flambda_type0_core.get_alias ty with
-          | None
-          | Some (Const _ | Discriminant _) -> ty
-          | Some (Name alias) ->
-            let alias_is_defined_name =
-              Name_occurrences.mem defined_names (Name alias)
-            in
-            let alias_is_not_used_on_rhs =
-              Name_occurrences.mem free_names_minus_defined_names'
-                (Name alias)
-            in
-            if alias_is_defined_name && alias_is_not_used_on_rhs
-            then resolve_aliases (find_exn t alias)
-            else ty
-        in
-        resolve_aliases ty)
-      t.equations
-  in
-  let t =
-    { t with equations; }
-  in
-  (* CR mshinwell: We can probably avoid re-computing this by calculating
-     it as we go along, just above. *)
-  let free_names_minus_defined_names =
-    free_names_minus_defined_names t
-  in
-  let unused_defined_names =
-    Name_occurrences.diff defined_names free_names_minus_defined_names
-  in
-  let allowed =
-    Name_occurrences.diff free_names_minus_defined_names
-      unused_defined_names
-  in
-  restrict_to_names t allowed
+let defined_names t = t.defined_names
 
 let add_definition t name kind =
   if Name.Map.mem name t.defined_names then begin
-    Misc.fatal_errorf "Typing environment extension already binds \
-        name %a:@ %a"
+    Misc.fatal_errorf "Environment extension already binds name %a:@ %a"
       Name.print name
       print t
   end;
-  let t =
-    { t with
-      defined_names = Name.Map.add name kind t.defined_names
-    }
-  in
-  tidy t
-
-let add_equation t name ty =
-  if Name.Map.mem name t.equations then begin
-    Misc.fatal_errorf "Equation on %a already exists in@ %a"
-      Name.print name
-      print t
-  end;
-  (* CR mshinwell: Must be much more careful about when [tidy] is called *)
-  let t =
-    { t with
-      equations = Name.Map.add name ty t.equations;
-    }
-  in
-  tidy t
+  { t with
+    defined_names = Name.Map.add name kind t.defined_names
+  }
 
 let add_or_replace_equation t name ty =
   { t with
@@ -202,41 +88,29 @@ let add_or_replace_equation t name ty =
   }
 
 let meet env (t1 : t) (t2 : t) : t =
-  (* Care: as per comment in [Typing_env_extension.meet]. *)
+  (* Care: the domains of [t1] and [t2] are treated as contravariant.
+     As such, since this is [meet], we perform unions on the domains.
+     So if one of them is bottom, the result of meeting it with any other
+     level is that level, not bottom. *)
   if is_empty t1 then begin
     t2
   end else if is_empty t2 then begin
     t1
   end else begin
+    (* CR mshinwell: There may be a more efficient way of doing this. *)
     let env = Meet_env.env env in
     let env = Typing_env.increment_scope_level env in
-    (* The domains of the levels are treated as contravariant.  As such,
-       since this is [meet], we perform a union on the domains. *)
     let env = Typing_env.add_opened_env_extension env t1 in
     let env = Typing_env.add_opened_env_extension env t2 in
     let level = Typing_env.current_level env in
-    let t =
-      Typing_env.cut0 env ~unknown_if_defined_at_or_later_than:level
-    in
-    tidy t
+    Typing_env.cut env ~unknown_if_defined_at_or_later_than:level
   end
 
-let meet_equation t env name ty =
-  let t' =
-    { (empty ()) with
-      equations = Name.Map.singleton name ty;
-    }
+let erase_aliases t ~allowed =
+  let equations =
+    Name.Map.map (fun ty -> Type_erase_aliases.erase_aliases ty ~allowed)
+      t.equations
   in
-  let env = Meet_env.create env in
-  meet env t t'
-
-let defined_names t = t.defined_names
-
-let defined_names_set t =
-  Name.Set.fold (fun name defined_names ->
-      Bindable_name.Set.add (Name name) defined_names)
-    (Name.Map.keys t.defined_names)
-    Bindable_name.Set.empty
-
-let defined_names_in_order t =
-  Bindable_name.Set.elements (defined_names_set t)
+  { t with
+    equations;
+  }
