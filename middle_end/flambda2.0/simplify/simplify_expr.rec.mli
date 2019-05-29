d09cc3f37b7bdda80029151d3e5b79832716da6b
Mark Shinwell <mshinwell@gmail.com>
2019-05-29 12:56:28 +0100

    Use rec-module strategy for simplify

--- middle_end/flambda2.0/simplify/simplify_expr.rec.mli
+++ middle_end/flambda2.0/simplify/simplify_expr.rec.mli
@@ -15,27 +15,9 @@
 (**************************************************************************)
 
 [@@@ocaml.warning "+a-4-30-40-41-42"]
-(* CR mshinwell: Fix warning 60 *)
-[@@@ocaml.warning "-60"]
 
-module E = Simplify_env_and_result.Env
-
-module Outer_namespace = struct
-  module Simplify_expr = Simplify_expr
-  module Simplify_named = Simplify_named
-  module Simplify_program = Simplify_program
-  module Simplify_toplevel = Simplify_toplevel
-end
-
-module rec Simplify_expr : Simplify_expr_intf.S =
-  Outer_namespace.Simplify_expr.Make (Simplify_named)
-and Simplify_named : Simplify_named_intf.S =
-  Outer_namespace.Simplify_named.Make (Simplify_toplevel)
-and Simplify_program : Simplify_program_intf.S =
-  Outer_namespace.Simplify_program.Make (Simplify_named) (Simplify_toplevel)
-and Simplify_toplevel : Simplify_toplevel_intf.S =
-  Outer_namespace.Simplify_toplevel.Make (Simplify_expr)
-
-let run ~backend ~round program =
-  let env = E.create ~round ~backend in
-  Simplify_program.simplify_program env program
+val simplify_expr
+   : Simplify_env_and_result.Env.t
+  -> Simplify_env_and_result.Result.t
+  -> Flambda.Expr.t
+  -> Flambda.Expr.t * Simplify_env_and_result.Result.t
