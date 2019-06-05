7f6874d5e549949d28088148298a49d5a0eef10d
Mark Shinwell <mshinwell@gmail.com>
2019-06-05 09:04:10 +0100

    typing

--- middle_end/flambda2.0/typing/flambda_type0_core.rec.mli
+++ middle_end/flambda2.0/typing/flambda_type0_core.rec.mli
@@ -92,7 +92,10 @@ val set_of_closures
    : closures:Flambda_types.t Closure_id.Map.t
   -> Flambda_types.t
 
-val set_of_closures_containing_at_least : Closure_id.t -> Flambda_types.t
+val set_of_closures_containing_at_least
+   : Closure_id.t
+  -> closure_var:Variable.t
+  -> Flambda_types.t
 
 val alias_type_of : Flambda_kind.t -> Simple.t -> Flambda_types.t
 val alias_type_of_as_ty_fabricated : Simple.t -> Flambda_types.ty_fabricated
