30afa844008b9fc315f39f9b3e7d6832799e61c6
Mark Shinwell <mshinwell@gmail.com>
2019-06-10 10:44:20 +0100

    blocks

--- middle_end/flambda2.0/typing/structures/product.rec.mli
+++ middle_end/flambda2.0/typing/structures/product.rec.mli
@@ -14,26 +14,11 @@
 
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
-module Make (Index : Identifiable.S) : sig
-  type t
-
-  (** Create a product value given the indexes with associated components. *)
-  val create : Flambda_types.t Index.Map.t -> t
-
-  (* CR mshinwell: Change all of these to just [bottom : t]? *)
-  val create_bottom : unit -> t
-
-  (** Widen the product by adding as many fields, after any existing fields,
-      so that the product has the same number of fields as [to_match].  If the
-      supplied product already has at least that many fields then it is
-      returned unchanged. *)
-  val widen : t -> to_match:t -> t
-
-  include Type_structure_intf.S
-    with type t := t
+module Make (Index : Identifiable.S)
+  : Product_intf.S
+    with module Index := Index
     with type flambda_type := Flambda_types.t
     with type typing_env := Typing_env.t
     with type meet_env := Meet_env.t
     with type type_equality_env := Type_equality_env.t
     with type typing_env_extension := Typing_env_extension.t
-end
