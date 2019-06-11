acdd29fbb34a4073c6494710289b1127aa441d02
Mark Shinwell <mshinwell@gmail.com>
2019-06-11 15:41:36 +0100

    inlining

--- tests.ml
+++ tests.ml
@@ -1,20 +1,21 @@
+external ( + ) : int -> int -> int = "%addint"
+external ( < ) : 'a -> 'a -> bool = "%lessthan"
+
 let [@inline always] to_inline _x _y = 42
 
-(*
-let f x y =
-  match x with
+let f c m n x' y' =
+  let x = if c < 0 then x' else x' + 1 in
+  let y = if c < 0 then y' else y' + 1 in
+  match m with
   | None -> 0
   | Some a ->
-    match y with
+    match n with
     | None -> 1
     | Some b ->
-      to_inline a b
-*)
-
-external ( + ) : int -> int -> int = "%addint"
+      to_inline (x + y) (a + b)
 
 let rec length_aux len = function
     [] -> len
   | _::l -> length_aux (len + 1) l
 
-let length l = length_aux 0 l
+let length l = (length_aux [@inlined never]) 0 l
