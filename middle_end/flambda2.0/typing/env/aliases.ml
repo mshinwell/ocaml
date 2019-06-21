77a8659ad5b053dcca72a42e16f485ed11f840a3
Mark Shinwell <mshinwell@gmail.com>
2019-06-21 11:48:18 +0100

    names

--- middle_end/flambda2.0/typing/env/aliases.ml
+++ middle_end/flambda2.0/typing/env/aliases.ml
@@ -20,10 +20,125 @@ module Make (E : sig
 
   val defined_earlier : t -> than:t -> bool
   val implicitly_bound_and_canonical : t -> bool
+
+  module Order_within_equiv_class : sig
+    type t
+    include Identifiable.S with type t := t
+  end
+
+  val order_within_equiv_class : t -> Order_within_equiv_class.t
 end) = struct
+  module Aliases_of_canonical_element : sig
+    type t
+
+    val print : Format.formatter -> t -> unit
+
+    val invariant : t -> unit
+
+    val empty : t
+    val is_empty : t -> bool
+
+    val add : t -> E.t -> t
+
+    val find_earliest
+       : t
+      -> min_order_within_equiv_class:E.Order_within_equiv_class.t
+      -> E.t option
+
+    val all : t -> E.Set.t
+
+    val mem : t -> E.t -> bool
+
+    val union : t -> t -> t
+    val inter : t -> t -> t
+  end = struct
+    type t = {
+      aliases : E.Set.t E.Order_within_equiv_class.Map.t;
+      all : E.Set.t;
+    }
+
+    let invariant _t = ()
+
+    let print ppf t =
+      E.Order_within_equiv_class.Map.print E.Set.print ppf t
+
+    let empty = {
+      aliases = E.Order_within_equiv_class.Map.empty;
+      all = E.Set.empty;
+    }
+
+    let is_empty t = E.Set.is_empty t.all
+
+    let add t elt =
+      if E.Set.mem elt t.all then begin
+        Misc.fatal_errorf "%a already added to [Aliases_of_canonical_element]: \
+            %a"
+          E.print elt
+          print t
+      end;
+      let aliases =
+        E.Order_within_equiv_class.Map.update (E.order_within_equiv_class elt)
+          (function
+            | None -> E.Set.singleton elt
+            | Some elts ->
+              assert (not (E.Set.mem elt elts));
+              E.Set.add elt elts)
+      in
+      let all = E.Set.add elt t.all in
+      { aliases;
+        all;
+      }
+
+    let find_earliest t ~min_order_within_equiv_class =
+      match
+        E.Order_within_equiv_class.Map.find_first (fun order ->
+            E.Order_within_equiv_class.compare
+              order min_order_within_equiv_class >= 0)
+          t.aliases
+      with
+      | exception Not_found -> None
+      | Some elts -> E.Set.min_elt_opt elts
+
+    let mem t elt =
+      E.Set.mem elt t.all
+
+    let all t = t.all
+
+    let union t1 t2 =
+      let aliases =
+        E.Order_within_equiv_class.Map.merge (function
+            | None, None -> None
+            | Some elts, None | None, Some elts -> Some elts
+            | Some elts1, Some elts2 -> Some (E.Set.union elts1 elts2))
+          t1.aliases t2.aliases
+      in
+      let t =
+        { aliases;
+          all = E.Set.union t1.all t2.all;
+        }
+      in
+      invariant t;
+      t
+
+    let inter t1 t2 =
+      let aliases =
+        E.Order_within_equiv_class.Map.merge (function
+            | None, None | Some _, None | None, Some _ -> None
+            | Some elts1, Some elts2 -> Some (E.Set.inter elts1 elts2))
+          t1 t2
+      in
+      let t =
+        { aliases;
+          all = E.Set.inter t1.all t2.all;
+        }
+      in
+      invariant t;
+      t
+  end
+
   type t = {
     canonical_elements : E.t E.Map.t;
-    aliases_of_canonical_elements : E.t E.Order_for_canonicals.t Map.t E.Map.t;
+    aliases_of_canonical_elements : Aliases_of_canonical_element.t E.Map.t;
   }
 
   let print ppf { canonical_elements; aliases_of_canonical_elements; } =
@@ -33,7 +148,8 @@ end) = struct
         @[<hov 1>(aliases_of_canonical_elements@ %a)@]\
         )@]"
       (E.Map.print E.print) canonical_elements
-      (E.Map.print E.Set.print) aliases_of_canonical_elements
+      (E.Map.print Aliases_of_canonical_element.print)
+      aliases_of_canonical_elements
 
   let invariant t =
     if !Clflags.flambda_invariant_checks then begin
@@ -46,6 +162,16 @@ end) = struct
       end;
       let _all_aliases : E.Set.t =
         E.Map.fold (fun canonical_element aliases all_aliases ->
+            Aliases_of_canonical_element.invariant aliases;
+            let aliases = Aliases_of_canonical_element.all aliases in
+            if not (E.Set.for_all (fun elt ->
+              E.defined_earlier canonical_element ~than:elt) aliases)
+            then begin
+              Misc.fatal_errorf "Canonical element %a is not earlier than \
+                  all of its aliases:@ %a"
+                E.print canonical_element
+                print t
+            end;
             if E.Set.mem canonical_element aliases then begin
               Misc.fatal_errorf "Canonical element %a occurs in alias set:@ %a"
                 E.print canonical_element
@@ -106,17 +232,21 @@ end) = struct
       let aliases_of_canonical_element =
         get_aliases_of_canonical_element t ~canonical_element
       in
-      assert (not (E.Set.mem to_be_demoted aliases_of_canonical_element));
+      assert (not (Aliases_of_canonical_element.mem
+        aliases_of_canonical_element to_be_demoted));
       let aliases_of_to_be_demoted =
         get_aliases_of_canonical_element t ~canonical_element:to_be_demoted
       in
-      assert (not (E.Set.mem canonical_element aliases_of_to_be_demoted));
-      assert (E.Set.is_empty (E.Set.inter
-        aliases_of_canonical_element aliases_of_to_be_demoted));
+      assert (not (Aliases_of_canonical_element.mem
+        aliases_of_to_be_demoted canonical_element));
+      assert (Aliases_of_canonical_element.is_empty (
+        Aliases_of_canonical_element.inter
+          aliases_of_canonical_element aliases_of_to_be_demoted));
       let aliases =
-        aliases_of_canonical_element
-        |> E.Set.union aliases_of_to_be_demoted
-        |> E.Set.add to_be_demoted
+        Aliases_of_canonical_element.add
+          (Aliases_of_canonical_element.union aliases_of_to_be_demoted
+            aliases_of_canonical_element)
+          to_be_demoted
       in
       let aliases_of_canonical_elements =
         t.aliases_of_canonical_elements
@@ -251,17 +381,19 @@ end) = struct
       add_result;
     add_result, t
 
-  let get_canonical_element t element ~min_order_for_canonicals =
+  let get_canonical_element t element ~min_order_within_equiv_class =
     match E.Map.find element t.canonical_elements with
     | exception Not_found -> None
-    | canonical_elements ->
-      if E.Order_for_canonicals.compare
-          (E.order_for_canonicals canonical_element)
-          min_order_for_canonicals
+    | canonical_element ->
+      if E.Order_within_equiv_class.compare
+          (E.order_within_equiv_class canonical_element)
+          min_order_within_equiv_class
         >= 0
       then Some canonical_element
       else
-
+        let aliases = E.get_aliases_of_canonical_element t ~canonical_element in
+        Aliases_of_canonical_element.find_earliest aliases
+          ~min_order_within_equiv_class
 
   let get_aliases t element =
     match canonical t element with
@@ -269,7 +401,10 @@ end) = struct
       get_aliases_of_canonical_element t ~canonical_element
     | Alias_of_canonical { element = _; canonical_element; } ->
       assert (not (E.equal element canonical_element));
-      let aliases = get_aliases_of_canonical_element t ~canonical_element in
+      let aliases =
+        Aliases_of_canonical_element.all
+          (get_aliases_of_canonical_element t ~canonical_element)
+      in
       assert (E.Set.mem element aliases);
       E.Set.add canonical_element aliases
 end
