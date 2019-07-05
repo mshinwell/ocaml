17fa4a23e1ad599e759f50ed8adef23c6eac4a5d
Mark Shinwell <mshinwell@gmail.com>
2019-07-05 11:14:52 +0100

    work

--- middle_end/flambda2.0/simplify/env/continuation_uses_env_intf.ml
+++ middle_end/flambda2.0/simplify/env/continuation_uses_env_intf.ml
@@ -19,18 +19,10 @@
 module type S = sig
   type t
 
-  val add_continuation
-     : t
-    -> Continuation.t
-    -> definition_scope_level:Scope.t
-    -> Flambda_arity.t
-    -> t
-
-  val add_exn_continuation
-     : t
-    -> Exn_continuation.t
-    -> definition_scope_level:Scope.t
-    -> t
+  (** We don't have an interface that insists on adding continuations before
+      seeing their uses. This would be problematic when inserting wrappers,
+      where we have already advanced past the point at which such wrappers would
+      need to be defined, before knowing that a wrapper is needed. *)
 
   val record_continuation_use
      : t
@@ -39,23 +31,14 @@ module type S = sig
     -> arg_types:Flambda_type.t list
     -> t
 
-  val continuation_scope_level : t -> Continuation.t -> Scope.t
-
-  val exn_continuation_scope_level : t -> Exn_continuation.t -> Scope.t
-
   (* CR mshinwell: Add [record_exn_continuation_use]? *)
 
   val continuation_env_and_arg_types
      : t
     -> definition_typing_env:Flambda_type.Typing_env.t
     -> Continuation.t
+    -> Flambda_arity.t
     -> Flambda_type.Typing_env.t * (Flambda_type.t list)
 
   val num_continuation_uses : t -> Continuation.t -> int
-
-  val check_continuation_is_bound : t -> Continuation.t -> unit
-
-  val check_exn_continuation_is_bound : t -> Exn_continuation.t -> unit
-
-  val continuation_arity : t -> Continuation.t -> Flambda_arity.t
 end
