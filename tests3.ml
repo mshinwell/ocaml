6f55f8b3b22a51d5dbdf6fe8e157b0c710a3a183
Mark Shinwell <mshinwell@gmail.com>
2019-07-18 16:23:33 +0200

    Maybe fixed 2

--- tests3.ml
+++ tests3.ml
@@ -14,10 +14,10 @@ external ( < ) : 'a -> 'a -> bool = "%lessthan"
 external array_get: 'a array -> int -> 'a = "%array_safe_get"
 external array_set: 'a array -> int -> 'a -> unit = "%array_safe_set"
 
-(*
 let foo arr f i =
   array_set arr i (f (array_get arr i))
 
+(*
 (*
 let bar x =
   if x then if x then 42 else 1 else 0
