327361c5adf54abf06776809d3b76d56dd5b5059
Mark Shinwell <mshinwell@gmail.com>
2019-07-19 13:14:40 +0200

    notes

--- tests.ml
+++ tests.ml
@@ -2,6 +2,8 @@ external ( + ) : int -> int -> int = "%addint"
 external ( - ) : int -> int -> int = "%subint"
 external ( > ) : 'a -> 'a -> bool = "%greaterthan"
 external ( < ) : 'a -> 'a -> bool = "%lessthan"
+external array_get: 'a array -> int -> 'a = "%array_safe_get"
+external array_set: 'a array -> int -> 'a -> unit = "%array_safe_set"
 
 let [@inline always] to_inline _x _y = 42
 
@@ -79,10 +81,6 @@ let foo x =
   | B _ -> 1
   | C _ -> 2
 
-(*
-external array_get: 'a array -> int -> 'a = "%array_safe_get"
-*)
-
 let repeated_comparisons x _a =
 (*  let x = x + array_get a 3 in *)
   if x < 10 then
@@ -90,3 +88,20 @@ let repeated_comparisons x _a =
     else 1000
   else
     2
+
+let foo arr f i =
+  array_set arr i (f (array_get arr i))
+
+let f c m n x' y' =
+  let x = if c < 0 then x' else x' + 10 in
+  let y = if c < 0 then y' else y' + 20 in
+  x + y
+(*
+  match m with
+  | None -> 0
+  | Some a ->
+    match n with
+    | None -> 1
+    | Some b ->
+      to_inline (x + y) (a + b)
+*)
--- tests3.ml
+++ tests3.ml
@@ -1,12 +1,3 @@
-(*type t =
-  | A
-  | B of int
-  | C of int
-
-let foo () = A
-let foo () = A
-*)
-
 external ( + ) : int -> int -> int = "%addint"
 external ( - ) : int -> int -> int = "%subint"
 external ( > ) : 'a -> 'a -> bool = "%greaterthan"
@@ -17,24 +8,25 @@ external array_set: 'a array -> int -> 'a -> unit = "%array_safe_set"
 let foo arr f i =
   array_set arr i (f (array_get arr i))
 
-(*
-(*
-let bar x =
-  if x then if x then 42 else 1 else 0
-*)
-*)
-let [@inline always] to_inline _x _y = 42
-
 let f c m n x' y' =
   let x = if c < 0 then x' else x' + 10 in
   let y = if c < 0 then y' else y' + 20 in
   x + y
-(*
-  match m with
-  | None -> 0
-  | Some a ->
-    match n with
-    | None -> 1
-    | Some b ->
-      to_inline (x + y) (a + b)
+
+(* Notes:
+
+- Extra continuation required when function calls in branches
+- Are the extra parameters removed if unused? - Looks like it
+
+For unboxing:
+- extra_params_and_args needs to go first (maybe it always should?)
+- Ordering imposed on parameters?
+- extra_params needs to be able to add let bindings e.g. to get the field
+before Apply_cont for unboxing a pair
+- Presumably need to stop at the turn-around point when simplifying the
+handler and make some decision.  Sounds like Cmmgen actually does this for
+checking allocation.
+
+- Stop creating exception continuations that just call other ones
+
 *)
