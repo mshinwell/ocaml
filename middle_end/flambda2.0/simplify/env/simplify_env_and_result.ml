60ddd23a2dd87bcef04e4b54b984cfd8933634a4
Mark Shinwell <mshinwell@gmail.com>
2019-08-06 17:45:09 +0100

    fix

--- middle_end/flambda2.0/simplify/env/simplify_env_and_result.ml
+++ middle_end/flambda2.0/simplify/env/simplify_env_and_result.ml
@@ -376,6 +376,8 @@ end = struct
         Continuation.print cont
         Continuation.print alias_for
     end;
+(* CR mshinwell: This should check that they are either both exn handlers
+   or both non-exn handlers
     if Continuation.is_exn cont || Continuation.is_exn alias_for then begin
       Misc.fatal_errorf "Cannot alias exception handlers: %a (exn handler? %b) \
           as alias for %a (exn handler? %b)"
@@ -384,6 +386,7 @@ end = struct
         Continuation.print alias_for
         (Continuation.is_exn alias_for)
     end;
+*)
     let alias_for = resolve_continuation_aliases t alias_for in
     let continuation_aliases =
       Continuation.Map.add cont alias_for t.continuation_aliases
