cad5c207bf9a70faf09a1164eead980635a1d473
Mark Shinwell <mshinwell@gmail.com>
2019-08-07 11:57:36 +0100

    Fix for partial app

---
+++ tests8.ml
@@ -0,0 +1,10 @@
+type +'a node =
+  | Nil
+  | Cons of 'a * 'a t
+
+and 'a t = unit -> 'a node
+
+let rec map f seq () =
+  match seq () with
+  | Nil -> Nil
+  | Cons (x, next) -> Cons (f x, map f next)
