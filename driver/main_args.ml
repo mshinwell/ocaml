8096a15a4c8059f6dba0ac6fbe63c587aff93daf
Greta Yorsh <gyorsh@janestreet.com>
2020-04-24 16:24:26 +0100

    ocaml#???? from gretay-js/????? Tracing probes

--- driver/main_args.ml
+++ driver/main_args.ml
@@ -287,6 +287,22 @@ let mk_no_keep_locs f =
   "-no-keep-locs", Arg.Unit f, " Do not keep locations in .cmi files"
 ;;
 
+let mk_probes f =
+  if Config.probes then
+    "-probes", Arg.Unit f, " Emit tracing probes as specified by [%%probe ..]"
+  else
+    let err () =
+      raise (Arg.Bad "OCaml has been configured without support for \
+                      tracing probes: -probes not available.")
+    in
+    "-probes", Arg.Unit err, " (option not available)"
+;;
+
+let mk_no_probes f =
+    "-no-probes", Arg.Unit f, " Ignore [%%probe ..]"
+;;
+
+
 let mk_labels f =
   "-labels", Arg.Unit f, " Use commuting label mode"
 ;;
@@ -1105,6 +1121,8 @@ module type Optcomp_options = sig
   val _afl_instrument : unit -> unit
   val _afl_inst_ratio : int -> unit
   val _function_sections : unit -> unit
+  val _probes : unit -> unit
+  val _no_probes : unit -> unit
 end;;
 
 module type Opttop_options = sig
@@ -1333,6 +1351,8 @@ struct
     mk_g_opt F._g;
     mk_function_sections F._function_sections;
     mk_stop_after ~native:true F._stop_after;
+    mk_probes F._probes;
+    mk_no_probes F._no_probes;
     mk_i F._i;
     mk_I F._I;
     mk_impl F._impl;
@@ -1925,6 +1945,8 @@ module Default = struct
          OCaml 4.08.0"
     let _shared () = shared := true; dlcode := true
     let _v () = print_version_and_library "native-code compiler"
+    let _no_probes = clear probes
+    let _probes = set probes
   end
 
   module Odoc_args = struct
