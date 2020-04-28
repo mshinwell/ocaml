ffe75f0973bb0cb5a1a23c64d4dc4f40b20f8e3d
Greta Yorsh <gyorsh@janestreet.com>
2020-04-28 14:43:29 +0100

    command line flag to control if probes are emitted or ignored

--- driver/main_args.ml
+++ driver/main_args.ml
@@ -311,6 +311,15 @@ let mk_no_keep_locs f =
   "-no-keep-locs", Arg.Unit f, " Do not keep locations in .cmi files"
 ;;
 
+let mk_probes f =
+    "-probes", Arg.Unit f, " Emit tracing probes as specified using [%%probe ..]"
+;;
+
+let mk_no_probes f =
+    "-no-probes", Arg.Unit f, " Ignore [%%probe ..]"
+;;
+
+
 let mk_labels f =
   "-labels", Arg.Unit f, " Use commuting label mode"
 ;;
@@ -1120,6 +1129,8 @@ module type Optcomp_options = sig
   val _afl_inst_ratio : int -> unit
   val _function_sections : unit -> unit
   val _save_ir_after : string -> unit
+  val _probes : unit -> unit
+  val _no_probes : unit -> unit
 end;;
 
 module type Opttop_options = sig
@@ -1347,6 +1358,8 @@ struct
     mk_stop_after ~native:true F._stop_after;
     mk_save_ir_after ~native:true F._save_ir_after;
     mk_start_from ~native:true F._start_from;
+    mk_probes F._probes;
+    mk_no_probes F._no_probes;
     mk_i F._i;
     mk_I F._I;
     mk_impl F._impl;
@@ -1950,6 +1963,8 @@ module Default = struct
          OCaml 4.08.0"
     let _shared () = shared := true; dlcode := true
     let _v () = print_version_and_library "native-code compiler"
+    let _no_probes = clear probes
+    let _probes = set probes
   end
 
   module Odoc_args = struct
--- lambda/translcore.ml
+++ lambda/translcore.ml
@@ -604,22 +604,21 @@ and transl_exp0 ~scopes e =
                !transl_module ~scopes Tcoerce_none None od.open_expr, body)
       end
   | Texp_probe {name; handler=exp} ->
+    if !Clflags.probes then begin
       let lam = transl_exp ~scopes exp in
       let map =
         Ident.Set.fold (fun v acc -> Ident.Map.add v (Ident.rename v) acc)
           (free_variables lam)
-          Ident.Map.empty
-      in
+          Ident.Map.empty in
       let arg_idents, param_idents = Ident.Map.bindings map |> List.split in
       let body = Lambda.rename map lam in
-      let attr =
-        { inline = Never_inline;
-          specialise = Always_specialise;
-          local = Never_local;
-          is_a_functor = false;
-          stub = false;
-        }
-      in
+      let attr = {
+        inline = Never_inline;
+        specialise = Always_specialise;
+        local = Never_local;
+        is_a_functor = false;
+        stub = false;
+      } in
       let handler =
         { kind = Curried;
           params = List.map (fun v -> v, Pgenval) param_idents;
@@ -627,33 +626,40 @@ and transl_exp0 ~scopes e =
           body;
           loc = of_raw_location ~scopes exp.exp_loc;
           attr;
-        }
-      in
+        } in
       let funcid = Ident.create_local ("probe_handler_" ^ name) in
       let app =
-        { ap_func = Lvar funcid;
+        {
+          ap_func = Lvar funcid;
           ap_args = List.map (fun id -> Lvar id) arg_idents;
           ap_loc = of_raw_location ~scopes e.exp_loc;
           ap_should_be_tailcall = false;
           ap_inlined = Never_inline;
           ap_specialised = Always_specialise;
           ap_probe = Some {name};
-        }
-      in
+        } in
       begin match Config.flambda with
       | true ->
-          Llet(Strict, Pgenval, funcid, Lfunction handler, Lapply app)
+        Llet(Strict, Pgenval, funcid,
+             Lfunction handler,
+             Lapply app)
       | false ->
-          (* Needs to be lifted to top level manually here,
-             because functions that contain other function declarations
-             are not inlined by Closure. For example, adding a probe into
-             the body of function foo will prevent foo from being inlined
-             into another function. *)
-          probe_handlers := (funcid, Lfunction handler)::!probe_handlers;
-          Lapply app
+        (* Needs to be lifted to top level manually here,
+           because functions that contain other function declarations
+           are not inlined. For example, adding a probe into the body
+           of function foo will prevent foo from being inlined into
+           another function. *)
+        probe_handlers := (funcid, Lfunction handler)::!probe_handlers;
+        Lapply app
       end
+    end else begin
+      lambda_unit
+    end
   | Texp_probe_is_enabled {name} ->
-      Lprim(Pprobe_is_enabled {name}, [], of_raw_location ~scopes e.exp_loc)
+    if !Clflags.probes then
+      Lprim(Pprobe_is_enabled {name}, [],  of_raw_location ~scopes e.exp_loc)
+    else
+      lambda_unit
 
 and pure_module m =
   match m.mod_desc with
