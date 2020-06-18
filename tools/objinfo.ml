de87e99928a0856ea5cd9c75712d27b628e5ed43
Mark Shinwell <mshinwell@gmail.com>
2020-06-18 11:19:06 +0100

    objinfo

--- tools/objinfo.ml
+++ tools/objinfo.ml
@@ -144,10 +144,13 @@ let print_cmx_infos (ui, crc) =
       Format.fprintf Format.std_formatter "  %a@." Printclambda.approx approx
     end else
       Format.printf "Clambda unit@.";
-  | Flambda2 info ->
+  | Flambda info ->
     Format.printf "Flambda2 unit:@.";
-    Format.fprintf Format.std_formatter "@[<hov 2>%a@]"
-      Flambda_cmx_format.print info
+    match info with
+    | None -> Format.printf " <opaque mode, or non-terminating initialiser>"
+    | Some info ->
+      Format.fprintf Format.std_formatter "@[<hov 2>%a@]"
+        Flambda_cmx_format.print info
   end;
   let pr_funs _ fns =
     List.iter (fun arity -> printf " %d" arity) fns in
