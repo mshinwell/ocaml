a2b2af06a882ed70f2ca9e1e4d96fa5c5c76580e
Mark Shinwell <mshinwell@gmail.com>
2020-07-13 17:23:15 +0100

    Fix test case so it does not need stdlib

--- flambdatest/mlexamples/genlex2.ml
+++ flambdatest/mlexamples/genlex2.ml
@@ -1,11 +1,15 @@
 (* Reduced from stdlib/genlex.ml *)
 
+external raise : exn -> 'a = "%raise"
+
+exception Error of string
+
 let foo f g =
   match f with
   | 'a' ->
     let _ =
       try g () with
-      | Stream.Failure -> raise (Stream.Error "")
+      | Failure _ -> raise (Error "")
     in
-    raise (Stream.Error "")
+    raise (Error "")
   | _ -> None
