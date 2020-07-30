mshinwell/ocaml

commit c666ec868fc8a318ef9242139156dc9e80932f1a

Remove R (#217)

middle_end/flambda/lifting/lift_inconstants.ml
@@ -22,7 +22,7 @@ let lift_non_closure_discovered_via_reified_continuation_param_types dacc
       var to_lift ~reified_continuation_params_to_symbols
       ~reified_definitions ~closure_symbols_by_set =
   let static_const = Reification.create_static_const to_lift in
-  match R.find_shareable_constant (DA.r dacc) static_const with
+  match DA.find_shareable_constant dacc static_const with
   | Some symbol ->
     let reified_continuation_params_to_symbols =
       Variable.Map.add var symbol reified_continuation_params_to_symbols
@@ -40,10 +40,7 @@ let lift_non_closure_discovered_via_reified_continuation_param_types dacc
           (Name.var var)
           (T.alias_type_of K.value (Simple.symbol symbol)))
     in
-    let dacc =
-      DA.map_r dacc ~f:(fun r ->
-        R.consider_constant_for_sharing r symbol static_const)
-    in
+    let dacc = DA.consider_constant_for_sharing dacc symbol static_const in
     let lifted_constant =
       (* The environment here may be used by [Sort_lifted_constants], but the
          type will not be. *)
@@ -352,7 +349,7 @@ let allowed_for_toplevel_lifting static_const =
 
 type reify_primitive_at_toplevel_result =
   | Lift of {
-    r : R.t;
+    dacc : DA.t;
     symbol : Symbol.t;
     static_const : Flambda.Static_const.t;
   }
@@ -381,7 +378,7 @@ let reify_primitive_at_toplevel dacc bound_var ty =
     else begin
       (* CR mshinwell: This should attempt to share the constant (see
          first function in this file for the code). *)
-      match R.find_shareable_constant (DA.r dacc) static_const with
+      match DA.find_shareable_constant dacc static_const with
       | Some symbol -> Shared symbol
       | None ->
         let symbol =
@@ -389,10 +386,8 @@ let reify_primitive_at_toplevel dacc bound_var ty =
             (Linkage_name.create
                (Variable.unique_name (Var_in_binding_pos.var bound_var)))
         in
-        let r =
-          R.consider_constant_for_sharing (DA.r dacc) symbol static_const
-        in
-        Lift { r; symbol; static_const; }
+        let dacc = DA.consider_constant_for_sharing dacc symbol static_const in
+        Lift { dacc; symbol; static_const; }
     end
   | Lift_set_of_closures _ | Simple _ | Cannot_reify | Invalid ->
     Cannot_reify

middle_end/flambda/lifting/lift_inconstants.mli
@@ -29,7 +29,7 @@ val lift_via_reification_of_continuation_param_types
 
 type reify_primitive_at_toplevel_result =
   | Lift of {
-    r : Simplify_env_and_result.Result.t;
+    dacc : Downwards_acc.t;
     symbol : Symbol.t;
     static_const : Flambda.Static_const.t;
   }

middle_end/flambda/simplify/simplify_expr.rec.ml
@@ -31,7 +31,6 @@ let rec simplify_let
   : 'a. DA.t -> Let.t -> 'a k -> Expr.t * 'a * UA.t
 = fun dacc let_expr k ->
   let module L = Flambda.Let in
-  let original_dacc = dacc in
   L.pattern_match let_expr ~f:(fun bindable_let_bound ~body ->
     let defining_expr = L.defining_expr let_expr in
     let place_lifted_constants_immediately =
@@ -214,7 +213,7 @@ let rec simplify_let
         in
         body, user_data, uacc
       end
-    | Reified { definition; bound_symbol; static_const; r; } ->
+    | Reified { definition; bound_symbol; static_const; dacc; } ->
       if place_lifted_constants_immediately then begin
         Misc.fatal_errorf "Did not expect [Simplify_named] to return \
             [Reified] (bound symbol %a)"
@@ -222,9 +221,8 @@ let rec simplify_let
       end;
       (* In this case, the defining expression was found to be constant,
          so we generate a "let symbol" binding.  The constant being bound
-         has not yet been added to either the [denv] or [r] components of
-         [dacc]; that will happen during simplification of the following built
-         [let_symbol_expr]. *)
+         has not yet been added to [dacc]; that will happen during
+         simplification of the following built [let_symbol_expr]. *)
       let let_symbol_expr =
         Expr.create_let_symbol bound_symbol Dominator static_const
           (Expr.create_pattern_let bindable_let_bound definition body)
@@ -232,10 +230,8 @@ let rec simplify_let
       (* We're effectively replacing one expression (the original [Let])
          with another here; the new expression will be completely
          re-simplified.  So we don't have to do anything relating to lifted
-         constants.  We use [original_dacc] to avoid having stale bindings
-         in the environment from the processing of the original [Let] binding.
-      *)
-      simplify_expr (DA.with_r original_dacc r) let_symbol_expr k
+         constants. *)
+      simplify_expr dacc let_symbol_expr k
     | Shared symbol ->
       if place_lifted_constants_immediately then begin
         Misc.fatal_errorf "Did not expect [Simplify_named] to return \
@@ -253,7 +249,7 @@ let rec simplify_let
       in
       (* Same comment as for the [Reified] case with regard to lifted
          constants. *)
-      simplify_expr original_dacc let_expr k)
+      simplify_expr dacc let_expr k)
 
 and simplify_one_continuation_handler :
  'a. DA.t
@@ -619,7 +615,6 @@ and simplify_recursive_let_cont_handlers
         let body, (handlers, user_data), uacc =
           simplify_expr dacc body (fun dacc_after_body ->
             let cont_uses_env = DA.continuation_uses_env dacc_after_body in
-            let r = DA.r dacc_after_body in
             let arg_types =
               (* We can't know a good type from the call types *)
               List.map T.unknown arity
@@ -655,7 +650,10 @@ and simplify_recursive_let_cont_handlers
                 code_age_relation_after_body
             in
             let denv = DE.with_typing_env denv typing_env in
-            let dacc = DA.create denv cont_uses_env r in
+            let dacc =
+              DA.with_denv dacc_after_body denv
+              |> DA.with_continuation_uses_env ~cont_uses_env
+            in
             let dacc = DA.add_lifted_constants dacc prior_lifted_constants in
             let dacc = DA.map_denv dacc ~f:DE.set_not_at_unit_toplevel in
             let handler, user_data, uacc =

middle_end/flambda/simplify/simplify_named.rec.ml
@@ -27,7 +27,7 @@ type simplify_named_result =
       definition : Named.t;
       bound_symbol : Bound_symbols.t;
       static_const : Static_const.t;
-      r : R.t;
+      dacc : DA.t;
     }
   | Shared of Symbol.t
 
@@ -87,13 +87,13 @@ let simplify_named0 dacc (bindable_let_bound : Bindable_let_bound.t)
         | Cannot_reify ->
           bindings_result [bindable_let_bound, defining_expr] dacc
         | Shared symbol -> Shared symbol
-        | Lift { r; symbol; static_const; } ->
+        | Lift { dacc; symbol; static_const; } ->
           let named = Named.create_simple (Simple.symbol symbol) in
           Reified
             { definition = named;
               bound_symbol = Singleton symbol;
               static_const;
-              r;
+              dacc;
             }
       end
       else
@@ -173,8 +173,7 @@ let simplify_named0 dacc (bindable_let_bound : Bindable_let_bound.t)
     let dacc =
       match bound_symbols with
       | Singleton symbol ->
-        DA.map_r dacc ~f:(fun r ->
-          R.consider_constant_for_sharing r symbol static_const)
+        DA.consider_constant_for_sharing dacc symbol static_const
       | Sets_of_closures _ -> dacc
     in
     let lifted_constant =

middle_end/flambda/simplify/simplify_named.rec.mli
@@ -27,7 +27,7 @@ type simplify_named_result = private
       definition : Named.t;
       bound_symbol : Bound_symbols.t;
       static_const :  Static_const.t;
-      r : R.t;
+      dacc : Downwards_acc.t;
     }
   | Shared of Symbol.t
 

.depend
@@ -4978,7 +4978,7 @@ middle_end/flambda/from_lambda/prepare_lambda.cmi : \
 middle_end/flambda/inlining/inlining_cost.cmo : \
     utils/targetint.cmi \
     lambda/switch.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/basic/simple.cmi \
     middle_end/flambda/terms/set_of_closures.cmi \
     middle_end/flambda/terms/function_declarations.cmi \
@@ -4994,7 +4994,7 @@ middle_end/flambda/inlining/inlining_cost.cmo : \
 middle_end/flambda/inlining/inlining_cost.cmx : \
     utils/targetint.cmx \
     lambda/switch.cmx \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/basic/simple.cmx \
     middle_end/flambda/terms/set_of_closures.cmx \
     middle_end/flambda/terms/function_declarations.cmx \
@@ -5008,11 +5008,11 @@ middle_end/flambda/inlining/inlining_cost.cmx : \
     utils/clflags.cmx \
     middle_end/flambda/inlining/inlining_cost.cmi
 middle_end/flambda/inlining/inlining_cost.cmi : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/terms/flambda_primitive.cmi \
     middle_end/flambda/terms/flambda.cmi
 middle_end/flambda/inlining/inlining_decision.cmo : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/compilenv_deps/rec_info.cmi \
     utils/numbers.cmi \
     utils/misc.cmi \
@@ -5023,7 +5023,7 @@ middle_end/flambda/inlining/inlining_decision.cmo : \
     utils/clflags.cmi \
     middle_end/flambda/inlining/inlining_decision.cmi
 middle_end/flambda/inlining/inlining_decision.cmx : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/compilenv_deps/rec_info.cmx \
     utils/numbers.cmx \
     utils/misc.cmx \
@@ -5034,7 +5034,7 @@ middle_end/flambda/inlining/inlining_decision.cmx : \
     utils/clflags.cmx \
     middle_end/flambda/inlining/inlining_decision.cmi
 middle_end/flambda/inlining/inlining_decision.cmi : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/compilenv_deps/rec_info.cmi \
     middle_end/flambda/basic/inline_attribute.cmi \
     middle_end/flambda/terms/function_declaration.cmi \
@@ -5044,7 +5044,7 @@ middle_end/flambda/inlining/inlining_transforms.cmo : \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
     middle_end/flambda/basic/trap_action.cmi \
     middle_end/flambda/simplify/simplify_import.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/basic/simple.cmi \
     middle_end/flambda/compilenv_deps/rec_info.cmi \
     middle_end/flambda/naming/name_permutation.cmi \
@@ -5061,7 +5061,7 @@ middle_end/flambda/inlining/inlining_transforms.cmx : \
     middle_end/flambda/naming/var_in_binding_pos.cmx \
     middle_end/flambda/basic/trap_action.cmx \
     middle_end/flambda/simplify/simplify_import.cmx \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/basic/simple.cmx \
     middle_end/flambda/compilenv_deps/rec_info.cmx \
     middle_end/flambda/naming/name_permutation.cmx \
@@ -5131,7 +5131,7 @@ middle_end/flambda/lifting/lift_inconstants.cmx : \
 middle_end/flambda/lifting/lift_inconstants.cmi : \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
     middle_end/flambda/compilenv_deps/symbol.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/basic/kinded_parameter.cmi \
     middle_end/flambda/types/flambda_type.cmi \
     middle_end/flambda/terms/flambda.cmi \
@@ -5793,7 +5793,7 @@ middle_end/flambda/simplify/simplify_common.cmi : \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
     middle_end/flambda/simplify/env/upwards_acc.cmi \
     middle_end/flambda/basic/symbol_scoping_rule.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/simplify/basic/reachable.cmi \
     middle_end/flambda/naming/name_mode.cmi \
     middle_end/flambda/types/flambda_type.cmi \
@@ -5911,7 +5911,7 @@ middle_end/flambda/simplify/simplify_import.cmo : \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
     middle_end/flambda/simplify/env/upwards_acc.cmi \
     middle_end/flambda/simplify/simplify_simple.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/naming/name_mode.cmi \
     middle_end/flambda/basic/kinded_parameter.cmi \
     middle_end/flambda/terms/flambda_unit.cmi \
@@ -5928,7 +5928,7 @@ middle_end/flambda/simplify/simplify_import.cmx : \
     middle_end/flambda/naming/var_in_binding_pos.cmx \
     middle_end/flambda/simplify/env/upwards_acc.cmx \
     middle_end/flambda/simplify/simplify_simple.cmx \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/naming/name_mode.cmx \
     middle_end/flambda/basic/kinded_parameter.cmx \
     middle_end/flambda/terms/flambda_unit.cmx \
@@ -5945,7 +5945,7 @@ middle_end/flambda/simplify/simplify_import.cmi : \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
     middle_end/flambda/simplify/env/upwards_acc.cmi \
     middle_end/flambda/simplify/simplify_simple.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/naming/name_mode.cmi \
     middle_end/flambda/basic/kinded_parameter.cmi \
     middle_end/flambda/terms/flambda_unit.cmi \
@@ -6362,7 +6362,7 @@ middle_end/flambda/simplify/env/continuation_uses_env.cmi : \
     middle_end/flambda/simplify/typing_helpers/continuation_uses.cmi \
     middle_end/flambda/basic/continuation.cmi
 middle_end/flambda/simplify/env/continuation_uses_env_intf.cmo : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/basic/kinded_parameter.cmi \
     middle_end/flambda/types/flambda_type.cmi \
     middle_end/flambda/simplify/env/continuation_use_kind.cmi \
@@ -6370,7 +6370,7 @@ middle_end/flambda/simplify/env/continuation_uses_env_intf.cmo : \
     middle_end/flambda/basic/continuation.cmi \
     middle_end/flambda/basic/apply_cont_rewrite_id.cmi
 middle_end/flambda/simplify/env/continuation_uses_env_intf.cmx : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/basic/kinded_parameter.cmx \
     middle_end/flambda/types/flambda_type.cmx \
     middle_end/flambda/simplify/env/continuation_use_kind.cmx \
@@ -6378,28 +6378,28 @@ middle_end/flambda/simplify/env/continuation_uses_env_intf.cmx : \
     middle_end/flambda/basic/continuation.cmx \
     middle_end/flambda/basic/apply_cont_rewrite_id.cmx
 middle_end/flambda/simplify/env/downwards_acc.cmo : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/types/flambda_type.cmi \
     middle_end/flambda/simplify/env/continuation_uses_env.cmi \
     middle_end/flambda/simplify/env/downwards_acc.cmi
 middle_end/flambda/simplify/env/downwards_acc.cmx : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/types/flambda_type.cmx \
     middle_end/flambda/simplify/env/continuation_uses_env.cmx \
     middle_end/flambda/simplify/env/downwards_acc.cmi
 middle_end/flambda/simplify/env/downwards_acc.cmi : \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/types/flambda_type.cmi \
     middle_end/flambda/simplify/env/continuation_uses_env_intf.cmo \
     middle_end/flambda/simplify/env/continuation_uses_env.cmi \
     middle_end/flambda/types/structures/code_age_relation.cmi
-middle_end/flambda/simplify/env/simplify_env_and_result.cmo : \
+middle_end/flambda/simplify/env/simplify_envs.cmo : \
     middle_end/flambda/compilenv_deps/variable.cmi \
     middle_end/flambda/basic/var_within_closure.cmi \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
     middle_end/flambda/compilenv_deps/symbol.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmo \
+    middle_end/flambda/simplify/env/simplify_envs_intf.cmo \
     middle_end/flambda/basic/simple.cmi \
     middle_end/flambda/basic/scope.cmi \
     middle_end/flambda/naming/name_mode.cmi \
@@ -6421,13 +6421,13 @@ middle_end/flambda/simplify/env/simplify_env_and_result.cmo : \
     middle_end/flambda/basic/closure_id.cmi \
     middle_end/flambda/terms/bound_symbols.cmi \
     middle_end/flambda/simplify/basic/apply_cont_rewrite.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi
-middle_end/flambda/simplify/env/simplify_env_and_result.cmx : \
+    middle_end/flambda/simplify/env/simplify_envs.cmi
+middle_end/flambda/simplify/env/simplify_envs.cmx : \
     middle_end/flambda/compilenv_deps/variable.cmx \
     middle_end/flambda/basic/var_within_closure.cmx \
     middle_end/flambda/naming/var_in_binding_pos.cmx \
     middle_end/flambda/compilenv_deps/symbol.cmx \
-    middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmx \
+    middle_end/flambda/simplify/env/simplify_envs_intf.cmx \
     middle_end/flambda/basic/simple.cmx \
     middle_end/flambda/basic/scope.cmx \
     middle_end/flambda/naming/name_mode.cmx \
@@ -6449,10 +6449,10 @@ middle_end/flambda/simplify/env/simplify_env_and_result.cmx : \
     middle_end/flambda/basic/closure_id.cmx \
     middle_end/flambda/terms/bound_symbols.cmx \
     middle_end/flambda/simplify/basic/apply_cont_rewrite.cmx \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi
-middle_end/flambda/simplify/env/simplify_env_and_result.cmi : \
-    middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmo
-middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmo : \
+    middle_end/flambda/simplify/env/simplify_envs.cmi
+middle_end/flambda/simplify/env/simplify_envs.cmi : \
+    middle_end/flambda/simplify/env/simplify_envs_intf.cmo
+middle_end/flambda/simplify/env/simplify_envs_intf.cmo : \
     middle_end/flambda/compilenv_deps/variable.cmi \
     middle_end/flambda/basic/var_within_closure.cmi \
     middle_end/flambda/naming/var_in_binding_pos.cmi \
@@ -6477,7 +6477,7 @@ middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmo : \
     middle_end/flambda/basic/closure_id.cmi \
     middle_end/flambda/terms/bound_symbols.cmi \
     middle_end/flambda/simplify/basic/apply_cont_rewrite.cmi
-middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmx : \
+middle_end/flambda/simplify/env/simplify_envs_intf.cmx : \
     middle_end/flambda/compilenv_deps/variable.cmx \
     middle_end/flambda/basic/var_within_closure.cmx \
     middle_end/flambda/naming/var_in_binding_pos.cmx \
@@ -6503,19 +6503,19 @@ middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmx : \
     middle_end/flambda/terms/bound_symbols.cmx \
     middle_end/flambda/simplify/basic/apply_cont_rewrite.cmx
 middle_end/flambda/simplify/env/upwards_acc.cmo : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/types/flambda_type.cmi \
     middle_end/flambda/simplify/env/downwards_acc.cmi \
     middle_end/flambda/types/structures/code_age_relation.cmi \
     middle_end/flambda/simplify/env/upwards_acc.cmi
 middle_end/flambda/simplify/env/upwards_acc.cmx : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/types/flambda_type.cmx \
     middle_end/flambda/simplify/env/downwards_acc.cmx \
     middle_end/flambda/types/structures/code_age_relation.cmx \
     middle_end/flambda/simplify/env/upwards_acc.cmi
 middle_end/flambda/simplify/env/upwards_acc.cmi : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/simplify/env/downwards_acc.cmi \
     middle_end/flambda/types/structures/code_age_relation.cmi
  \
@@ -6534,7 +6534,7 @@ middle_end/flambda/simplify/env/upwards_acc.cmi : \
     middle_end/flambda/basic/continuation.cmx
 middle_end/flambda/simplify/typing_helpers/continuation_uses.cmo : \
     middle_end/flambda/compilenv_deps/symbol.cmi \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/basic/scope.cmi \
     middle_end/flambda/simplify/typing_helpers/one_continuation_use.cmi \
     middle_end/flambda/basic/name.cmi \
@@ -6551,7 +6551,7 @@ middle_end/flambda/simplify/typing_helpers/continuation_uses.cmo : \
     middle_end/flambda/simplify/typing_helpers/continuation_uses.cmi
 middle_end/flambda/simplify/typing_helpers/continuation_uses.cmx : \
     middle_end/flambda/compilenv_deps/symbol.cmx \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmx \
+    middle_end/flambda/simplify/env/simplify_envs.cmx \
     middle_end/flambda/basic/scope.cmx \
     middle_end/flambda/simplify/typing_helpers/one_continuation_use.cmx \
     middle_end/flambda/basic/name.cmx \
@@ -6567,7 +6567,7 @@ middle_end/flambda/simplify/typing_helpers/continuation_uses.cmx : \
     middle_end/flambda/basic/apply_cont_rewrite_id.cmx \
     middle_end/flambda/simplify/typing_helpers/continuation_uses.cmi
 middle_end/flambda/simplify/typing_helpers/continuation_uses.cmi : \
-    middle_end/flambda/simplify/env/simplify_env_and_result.cmi \
+    middle_end/flambda/simplify/env/simplify_envs.cmi \
     middle_end/flambda/simplify/typing_helpers/one_continuation_use.cmi \
     middle_end/flambda/basic/kinded_parameter.cmi \
     middle_end/flambda/types/flambda_type.cmi \

Makefile
@@ -343,8 +343,8 @@ MIDDLE_END_FLAMBDA_SIMPLIFY=\
   middle_end/flambda/simplify/env/continuation_env_and_param_types.cmo \
   middle_end/flambda/simplify/basic/continuation_in_env.cmo \
   middle_end/flambda/simplify/basic/reachable.cmo \
-  middle_end/flambda/simplify/env/simplify_env_and_result_intf.cmo \
-  middle_end/flambda/simplify/env/simplify_env_and_result.cmo \
+  middle_end/flambda/simplify/env/simplify_envs_intf.cmo \
+  middle_end/flambda/simplify/env/simplify_envs.cmo \
   middle_end/flambda/simplify/typing_helpers/one_continuation_use.cmo \
   middle_end/flambda/simplify/typing_helpers/continuation_uses.cmo \
   middle_end/flambda/simplify/env/continuation_uses_env.cmo \

middle_end/flambda/inlining/inlining_cost.ml
@@ -18,7 +18,7 @@
 
 open! Flambda.Import
 
-module DE = Simplify_env_and_result.Downwards_env
+module DE = Simplify_envs.Downwards_env
 
 let arch32 = Targetint.size = 32 (* are we compiling for a 32-bit arch *)
 let arch64 = Targetint.size = 64 (* are we compiling for a 64-bit arch *)

commit 3be8d13eb1877be54d5ecfc4cf4923309734f490

Fix for bug introduced in #217

middle_end/flambda/lifting/lift_inconstants.ml
@@ -350,7 +350,6 @@ let allowed_for_toplevel_lifting static_const =
 
 type reify_primitive_at_toplevel_result =
   | Lift of {
-    dacc : DA.t;
     symbol : Symbol.t;
     static_const : Flambda.Static_const.t;
   }
@@ -377,8 +376,6 @@ let reify_primitive_at_toplevel dacc bound_var ty =
     then
       Cannot_reify
     else begin
-      (* CR mshinwell: This should attempt to share the constant (see
-         first function in this file for the code). *)
       match DA.find_shareable_constant dacc static_const with
       | Some symbol -> Shared symbol
       | None ->
@@ -387,8 +384,7 @@ let reify_primitive_at_toplevel dacc bound_var ty =
             (Linkage_name.create
                (Variable.unique_name (Var_in_binding_pos.var bound_var)))
         in
-        let dacc = DA.consider_constant_for_sharing dacc symbol static_const in
-        Lift { dacc; symbol; static_const; }
+        Lift { symbol; static_const; }
     end
   | Lift_set_of_closures _ | Simple _ | Cannot_reify | Invalid ->
     Cannot_reify

middle_end/flambda/lifting/lift_inconstants.mli
@@ -29,7 +29,6 @@ val lift_via_reification_of_continuation_param_types
 
 type reify_primitive_at_toplevel_result =
   | Lift of {
-    dacc : Downwards_acc.t;
     symbol : Symbol.t;
     static_const : Flambda.Static_const.t;
   }

middle_end/flambda/simplify/simplify_expr.rec.ml
@@ -31,6 +31,7 @@ let rec simplify_let
   : 'a. DA.t -> Let.t -> 'a k -> Expr.t * 'a * UA.t
 = fun dacc let_expr k ->
   let module L = Flambda.Let in
+  let original_dacc = dacc in
   L.pattern_match let_expr ~f:(fun bindable_let_bound ~body ->
     let defining_expr = L.defining_expr let_expr in
     let place_lifted_constants_immediately =
@@ -213,7 +214,7 @@ let rec simplify_let
         in
         body, user_data, uacc
       end
-    | Reified { definition; bound_symbol; static_const; dacc; } ->
+    | Reified { definition; bound_symbol; symbol; static_const; } ->
       if place_lifted_constants_immediately then begin
         Misc.fatal_errorf "Did not expect [Simplify_named] to return \
             [Reified] (bound symbol %a)"
@@ -230,7 +231,15 @@ let rec simplify_let
       (* We're effectively replacing one expression (the original [Let])
          with another here; the new expression will be completely
          re-simplified.  So we don't have to do anything relating to lifted
-         constants. *)
+         constants.
+         We have to revert to [original_dacc] as types in [dacc] may have
+         been affected (due to aliases) in ways that would be wrong to
+         propagate.  We do however augment [original_dacc] so that it knows
+         [symbol], which we have freshly generated, is eligible for
+         sharing. *)
+      let dacc =
+        DA.consider_constant_for_sharing original_dacc symbol static_const
+      in
       simplify_expr dacc let_symbol_expr k
     | Shared symbol ->
       if place_lifted_constants_immediately then begin
@@ -248,8 +257,8 @@ let rec simplify_let
           body
       in
       (* Same comment as for the [Reified] case with regard to lifted
-         constants. *)
-      simplify_expr dacc let_expr k)
+         constants and [original_dacc]. *)
+      simplify_expr original_dacc let_expr k)
 
 and simplify_one_continuation_handler :
  'a. DA.t

middle_end/flambda/simplify/simplify_named.rec.ml
@@ -26,8 +26,8 @@ type simplify_named_result =
   | Reified of {
       definition : Named.t;
       bound_symbol : Bound_symbols.t;
+      symbol : Symbol.t;
       static_const : Static_const.t;
-      dacc : DA.t;
     }
   | Shared of Symbol.t
 
@@ -88,13 +88,13 @@ let simplify_named0 dacc (bindable_let_bound : Bindable_let_bound.t)
       | Cannot_reify ->
         bindings_result [bindable_let_bound, defining_expr] dacc
       | Shared symbol -> Shared symbol
-      | Lift { dacc; symbol; static_const; } ->
+      | Lift { symbol; static_const; } ->
         let named = Named.create_simple (Simple.symbol symbol) in
         Reified
           { definition = named;
             bound_symbol = Singleton symbol;
+            symbol;
             static_const;
-            dacc;
           }
     end
     else

middle_end/flambda/simplify/simplify_named.rec.mli
@@ -26,8 +26,8 @@ type simplify_named_result = private
   | Reified of {
       definition : Named.t;
       bound_symbol : Bound_symbols.t;
+      symbol : Symbol.t;
       static_const :  Static_const.t;
-      dacc : Downwards_acc.t;
     }
   | Shared of Symbol.t
 