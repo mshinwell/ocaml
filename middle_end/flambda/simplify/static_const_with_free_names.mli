28699190f2a72ba8488e05d58bbb830c0f997975
Mark Shinwell <mshinwell@gmail.com>
2020-10-29 15:47:43 +0000

    lets

---
+++ middle_end/flambda/simplify/static_const_with_free_names.mli
@@ -0,0 +1,26 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2013--2020 OCamlPro SAS                                    *)
+(*   Copyright 2014--2020 Jane Street Group LLC                           *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.warning "+a-30-40-41-42"]
+
+open! Flambda.Import
+
+type t
+
+val create : Static_const.t -> Name_occurrences.t -> t
+
+val const : t -> Static_const.t
+val free_names : t -> Name_occurrences.t
