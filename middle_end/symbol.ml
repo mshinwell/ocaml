f5cd3f58a60ae284754de6a15d4e67dc7bb85d66
Mark Shinwell <mshinwell@gmail.com>
2021-04-02 18:47:39 +0100

    Linkage_name removal

--- middle_end/symbol.ml
+++ middle_end/symbol.ml
@@ -6,7 +6,7 @@
 (*           Mark Shinwell and Leo White, Jane Street Europe              *)
 (*                                                                        *)
 (*   Copyright 2013--2016 OCamlPro SAS                                    *)
-(*   Copyright 2014--2016 Jane Street Group LLC                           *)
+(*   Copyright 2014--2021 Jane Street Group LLC                           *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -14,15 +14,15 @@
 (*                                                                        *)
 (**************************************************************************)
 
-[@@@ocaml.warning "+a-4-9-30-40-41-42-66"]
+[@@@ocaml.warning "+a-9-30-40-41-42"]
+
+module CU = Compilation_unit
 
 type t = {
   compilation_unit : Compilation_unit.t;
-  linkage_name : Linkage_name.t;
+  linkage_name : string;
   hash : int;
 }
-
-let linkage_name t = t.linkage_name
 
 include Identifiable.Make (struct
   type nonrec t = t
@@ -35,21 +35,50 @@ include Identifiable.Make (struct
       else
         (* Linkage names are unique across a whole project, so just comparing
            those is sufficient. *)
-        Linkage_name.compare t1.label t2.label
+        String.compare t1.linkage_name t2.linkage_name
 
   let equal t1 t2 = compare t1 t2 = 0
-  let output chan t = Linkage_name.output chan (label t)
+  let output chan t = output_string chan t.linkage_name
   let hash { hash; } = hash
-  let print ppf t = Linkage_name.print ppf (label t)
+  let print ppf t = Format.pp_print_string ppf t.linkage_name
 end)
 
-let of_global_linkage compilation_unit label =
-  let hash = Linkage_name.hash label in
-  Linkage { compilation_unit; hash; label }
+let linkage_name t = t.linkage_name
+let compilation_unit t = t.compilation_unit
+
+let caml_symbol_prefix = "caml"
+let separator = "__"
+
+let linkage_name_for_compilation_unit comp_unit =
+  let name = CU.Name.to_string (CU.name comp_unit) in
+  let for_pack_prefix = CU.for_pack_prefix comp_unit in
+  let suffix =
+    if CU.Prefix.is_empty for_pack_prefix then name
+    else
+      let pack_names =
+        CU.Prefix.to_list_outermost_pack_first for_pack_prefix
+        |> List.map CU.Name.to_string
+      in
+      String.concat separator (pack_names @ [name])
+  in
+  caml_symbol_prefix ^ suffix
+
+let linkage_name_for_current_unit () =
+  for_compilation_unit (CU.get_current_exn ())
+
+let linkage_name_for_ident id =
+  if Ident.is_predef id
+  then "caml_exn_" ^ Ident.name id
+  else
+    match Ident.compilation_unit_of_global_or_predef_ident id with
+    | Some comp_unit -> for_compilation_unit comp_unit
+    | None ->
+      assert (not (Ident.is_global id));
+      (for_compilation_unit (CU.get_current_exn ()))
+        ^ separator ^ Ident.unique_name id
 
 let for_ident id =
-  let linkage_name = Linkage_name.for_ident id in
-  let hash = Linkage_name.hash linkage_name in
+  let linkage_name = linkage_name_for_ident id in
   let compilation_unit =
     if Ident.is_global_or_predef id
     then Ident.compilation_unit_of_global_or_predef_ident id
@@ -57,26 +86,57 @@ let for_ident id =
   in
   { compilation_unit;
     linkage_name;
-    hash;
+    hash = Hashtbl.hash linkage_name;
   }
 
-let for_variable var =
-  let linkage_name = Linkage_name.Flambda.for_variable var in
-  { compilation_unit = Variable.get_compilation_unit var;
-    linkage_name;
-    hash = Linkage_name.hash linkage_name;
-  }
+let is_in_current_unit t =
+  let prefix = linkage_name_for_current_unit () in
+  String.equal t.linkage_name prefix
+    || String.starts_with ~prefix:(prefix ^ separator) t.linkage_name
 
-let for_closure closure_id =
-  let linkage_name = Linkage_name.Flambda.for_closure closure_id in
-  { compilation_unit = Closure_id.get_compilation_unit closure_id;
-    linkage_name;
-    hash = Linkage_name.hash linkage_name;
-  }
+module Flambda = struct
+  let for_variable var =
+    let compilation_unit = Variable.get_compilation_unit var in
+    let linkage_name =
+      (for_compilation_unit compilation_unit)
+        ^ separator ^ Variable.unique_name var
+    in
+    { compilation_unit;
+      linkage_name;
+      hash = Hashtbl.hash linkage_name;
+    }
 
-let for_code_of_closure closure_id =
-  let linkage_name = Linkage_name.Flambda.for_code_of_closure closure_id in
-  { compilation_unit = Closure_id.get_compilation_unit closure_id;
+  let for_closure closure_id =
+    let compilation_unit = Closure_id.get_compilation_unit closure_id in
+    let linkage_name =
+      (for_compilation_unit compilation_unit)
+        ^ separator ^ Closure_id.unique_name closure_id ^ "_closure"
+    in
+    { compilation_unit;
+      linkage_name;
+      hash = Hashtbl.hash linkage_name;
+    }
+
+  let for_code_of_closure closure_id =
+    let compilation_unit = Closure_id.get_compilation_unit closure_id in
+    let linkage_name =
+      (for_compilation_unit compilation_unit)
+        ^ separator ^ Closure_id.unique_name closure_id
+    in
+    { compilation_unit;
+      linkage_name;
+      hash = Hashtbl.hash linkage_name;
+    }
+end
+
+let const_label = ref 0
+
+let for_new_const_in_current_unit () =
+  incr const_label;
+  let linkage_name =
+    linkage_name_for_current_unit () ^ separator ^ (Int.to_string !const_label)
+  in
+  { compilation_unit = Compilation_unit.get_current_exn ();
     linkage_name;
-    hash = Linkage_name.hash linkage_name;
+    hash = Hashtbl.hash linkage_name;
   }
--- middle_end/symbol.mli
+++ middle_end/symbol.mli
@@ -6,7 +6,7 @@
 (*           Mark Shinwell and Leo White, Jane Street Europe              *)
 (*                                                                        *)
 (*   Copyright 2013--2016 OCamlPro SAS                                    *)
-(*   Copyright 2014--2016 Jane Street Group LLC                           *)
+(*   Copyright 2014--2021 Jane Street Group LLC                           *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -14,35 +14,29 @@
 (*                                                                        *)
 (**************************************************************************)
 
-[@@@ocaml.warning "+a-4-9-30-40-41-42"]
+[@@@ocaml.warning "+a-30-40-41-42"]
 
-(** A symbol identifies a constant provided by either:
-    - another compilation unit; or
-    - a top-level module.
+(** Symbols that identify statically-allocated code or data. *)
 
-    * [sym_unit] is the compilation unit containing the value.
-    * [sym_label] is the linkage name of the variable.
-
-    The label must be globally unique: two compilation units linked in the
-    same program must not share labels. *)
-
-include Identifiable.S
+type t
 
 (** It is assumed that the provided [Ident.t] is in the current unit unless
     it is [Global] or [Predef]. *)
 val for_ident : Ident.t -> t
 
-val for_variable : Variable.t -> t
-val for_closure : Closure_id.t -> t
-val for_code_of_closure : Closure_id.t -> t
+val for_new_const_in_current_unit : unit -> t
 
-(* Create the symbol without prefixing with the compilation unit.
-   Used for global symbols like predefined exceptions *)
-val of_global_linkage : Compilation_unit.t -> Linkage_name.t -> t
+module Flambda : sig
+  val for_variable : Variable.t -> t
+  val for_closure : Closure_id.t -> t
+  val for_code_of_closure : Closure_id.t -> t
+end
 
 val compilation_unit : t -> Compilation_unit.t
-val linkage_name : t -> Linkage_name.t
+val linkage_name : t -> string
 
-val print_opt : Format.formatter -> t option -> unit
+include Identifiable.S with type t := t
 
-val compare_lists : t list -> t list -> int
+val to_string : t -> string
+
+val is_in_current_unit : t -> bool
