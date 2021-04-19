002df4fa0ff587817b107f699649471b680cdaed
Guillaume Bury <guillaume.bury@gmail.com>
2021-04-19 15:54:09 +0200

    Refactor the unboxing decision code

--- middle_end/flambda/unboxing/unbox_continuation_params.mli
+++ middle_end/flambda/unboxing/unbox_continuation_params.mli
@@ -18,10 +18,25 @@
 
 open! Simplify_import
 
-val make_unboxing_decisions
-   : DE.t
-  -> arg_types_by_use_id:(TE.t * T.t) Apply_cont_rewrite_id.Map.t list
-  -> params:KP.t list
-  -> param_types:T.t list
-  -> Continuation_extra_params_and_args.t
-  -> DE.t * Continuation_extra_params_and_args.t
+type decisions
+
+val print : Format.formatter -> decisions -> unit
+
+val make_decisions
+  : continuation_is_recursive:bool
+  -> arg_types_by_use_id:
+       Continuation_env_and_param_types.arg_at_use
+         Apply_cont_rewrite_id.Map.t list
+  -> DE.t
+  -> KP.t list
+  -> T.t list
+  -> DE.t * decisions
+
+val compute_extra_params_and_args
+  : decisions
+  -> arg_types_by_use_id:
+       Continuation_env_and_param_types.arg_at_use
+         Apply_cont_rewrite_id.Map.t list
+  -> EPA.t
+  -> EPA.t
+
