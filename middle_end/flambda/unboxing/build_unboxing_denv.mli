1d8294ee7de9a93709c4069f9c391ace5ef62b6e
Mark Shinwell <mshinwell@gmail.com>
2021-04-26 14:38:38 +0100

    Add Build_unboxing_denv

---
+++ middle_end/flambda/unboxing/build_unboxing_denv.mli
@@ -0,0 +1,25 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2013--2020 OCamlPro SAS                                    *)
+(*   Copyright 2014--2020 Jane Street Group LLC                           *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.warning "+a-30-40-41-42"]
+
+open! Simplify_import
+
+val denv_of_decision
+   : DE.t
+  -> param_var:Variable.t
+  -> Unboxing_types.decision
+  -> DE.t
