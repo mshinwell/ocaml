98250f3eafca58f805428248d3eef9028913ac30
Mark Shinwell <mshinwell@gmail.com>
2021-04-29 10:40:45 +0100

    Minor code review changes

--- middle_end/flambda/unboxing/optimistic_unboxing_decision.ml
+++ middle_end/flambda/unboxing/optimistic_unboxing_decision.ml
@@ -18,33 +18,29 @@
 
 open! Simplify_import
 
-open Unboxing_types
-module U = Unboxers
+module U = Unboxing_types
+module Extra_param_and_args = U.Extra_param_and_args
 
-(* small helper used later *)
 let pp_tag print_tag ppf tag =
   if print_tag then Format.fprintf ppf "_%d" (Tag.to_int tag)
 
-(* Unfold a type into a decision tree *)
-(* ********************************** *)
-
-(* CR: make this a Clflag *)
+(* CR gbury: make this a Clflag *)
 let max_unboxing_depth = 1
 let unbox_numbers = true
 let unbox_blocks = true
 let unbox_variants = true
 let unbox_closures = true
 
-let make_optimistic_const_ctor () =
+let make_optimistic_const_ctor () : U.const_ctors =
   let is_int = Extra_param_and_args.create ~name:"is_int" in
   let unboxed_const_ctor =
     Extra_param_and_args.create ~name:"unboxed_const_ctor"
   in
-  let ctor = Unbox (Number (Naked_immediate, unboxed_const_ctor)) in
+  let ctor = U.Unbox (Number (Naked_immediate, unboxed_const_ctor)) in
   At_least_one { is_int; ctor; }
 
 let make_optimistic_number_decision tenv param_type
-      (decider : Unboxers.number_decider) : decision option =
+      (decider : Unboxers.number_decider) : U.decision option =
   match decider.prove_is_a_boxed_number tenv param_type with
   | Proved () ->
     let naked_number =
@@ -54,18 +50,18 @@ let make_optimistic_number_decision tenv param_type
   | Wrong_kind | Invalid | Unknown ->
     None
 
-let decide tenv param_type deciders : decision option =
+let decide tenv param_type deciders : U.decision option =
   List.find_map (make_optimistic_number_decision tenv param_type) deciders
 
 let deciders = [
-  U.Immediate.decider;
-  U.Float.decider;
-  U.Int32.decider;
-  U.Int64.decider;
-  U.Nativeint.decider;
+  Unboxers.Immediate.decider;
+  Unboxers.Float.decider;
+  Unboxers.Int32.decider;
+  Unboxers.Int64.decider;
+  Unboxers.Nativeint.decider;
 ]
 
-let rec make_optimistic_decision ~depth tenv ~param_type : decision =
+let rec make_optimistic_decision ~depth tenv ~param_type : U.decision =
   match decide tenv param_type deciders with
   | Some decision ->
     if unbox_numbers then decision else Do_not_unbox Incomplete_parameter_type
@@ -84,7 +80,7 @@ let rec make_optimistic_decision ~depth tenv ~param_type : decision =
         | Proved { const_ctors; non_const_ctors_with_sizes; }
             when unbox_variants ->
           let tag = Extra_param_and_args.create ~name:"tag" in
-          let constant_constructors =
+          let constant_constructors : U.const_ctors =
             match const_ctors with
             | Known set when Target_imm.Set.is_empty set -> Zero
             | Unknown | Known _ -> make_optimistic_const_ctor ()
@@ -126,7 +122,7 @@ and make_optimistic_fields
       (fun i -> Extra_param_and_args.create ~name:(field_name i))
   in
   let type_of_var (epa : Extra_param_and_args.t) =
-    Flambda_type.alias_type_of field_kind (Simple.var epa.param)
+    T.alias_type_of field_kind (Simple.var epa.param)
   in
   let field_types = List.map type_of_var field_vars in
   let tenv =
@@ -136,7 +132,7 @@ and make_optimistic_fields
     ) tenv field_vars
   in
   let shape =
-    Flambda_type.immutable_block ~is_unique:false tag
+    T.immutable_block ~is_unique:false tag
       ~field_kind ~fields:field_types
   in
   let env_extension =
@@ -147,7 +143,7 @@ and make_optimistic_fields
   in
   let tenv = TE.add_env_extension tenv env_extension in
   let fields =
-    List.map2 (fun epa var_type ->
+    List.map2 (fun epa var_type : U.field_decision ->
       let decision =
         make_optimistic_decision ~depth:(depth + 1) tenv ~param_type:var_type
       in
@@ -159,13 +155,14 @@ and make_optimistic_fields
 and make_optimistic_vars_within_closure
       ~depth tenv closures_entry =
   let map = T.Closures_entry.closure_var_types closures_entry in
-  Var_within_closure.Map.mapi (fun var_within_closure var_type ->
-    let epa =
-      Extra_param_and_args.create
-        ~name:(Var_within_closure.to_string var_within_closure)
-    in
-    let decision =
-      make_optimistic_decision ~depth:(depth + 1) tenv ~param_type:var_type
-    in
-    { epa; decision; }
-  ) map
+  Var_within_closure.Map.mapi
+    (fun var_within_closure var_type : U.field_decision ->
+      let epa =
+        Extra_param_and_args.create
+          ~name:(Var_within_closure.to_string var_within_closure)
+      in
+      let decision =
+        make_optimistic_decision ~depth:(depth + 1) tenv ~param_type:var_type
+      in
+      { epa; decision; })
+    map
