0024fbe15da15d5a68ffed939ffe53f80416edee
Mark Shinwell <mshinwell@gmail.com>
2021-06-02 11:00:35 +0100

    inline

--- middle_end/flambda/naming/name_occurrences.ml
+++ middle_end/flambda/naming/name_occurrences.ml
@@ -581,7 +581,7 @@ end = struct
       let map = N.Map.filter (fun name _ -> f name) map in
       if N.Map.is_empty map then Empty
       else Potentially_many map
-end [@@@inlined always]
+end [@@inline always]
 
 module For_names = For_one_variety_of_names (struct
   include Name
